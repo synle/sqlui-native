30e1c48b5dd22d6e2d3dd877663d2614c00e6f43
Sy Le <lenguyensy@gmail.com>
2022-01-24 12:54:03 -0800

    minor format prettier changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-unused-vars": ["warn", { "caughtErrors": "none", "argsIgnorePattern": "^_" } ]
-    }
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': ['warn', { caughtErrors: 'none', argsIgnorePattern: '^_' }],
+  },
 };
