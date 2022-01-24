9f17df3ac1a325404143a3c6f49d8b7bc3a16fc8
Sy Le <lenguyensy@gmail.com>
2022-01-24 12:54:03 -0800

    minor format prettier changes

--- mocked-server/index.ts
+++ mocked-server/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { RelationalDatabaseEngine } from './utils/RelationalDatabaseEngine';
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
 type ConnectionProps = {
   id: string;
@@ -59,30 +59,32 @@ ConnectionUtils.addConnection(`mariadb://root:password@localhost:33061`, 'sqlui_
 
 app.get('/api/connections', async (req, res) => {
   res.json(await ConnectionUtils.getConnections());
-})
+});
 
 app.get('/api/connection/:connectionId', async (req, res) => {
   res.json(await ConnectionUtils.getConnection(req.params?.connectionId));
-})
+});
 
 app.get('/api/connection/:connectionId/databases', async (req, res) => {
   const connection = await ConnectionUtils.getConnection(req.params?.connectionId);
   const engine = new RelationalDatabaseEngine(connection.connection);
   res.json(await engine.getDatabases());
-})
+});
 
 app.get('/api/connection/:connectionId/database/:databaseId/tables', async (req, res) => {
   const connection = await ConnectionUtils.getConnection(req.params?.connectionId);
   const engine = new RelationalDatabaseEngine(connection.connection);
   res.json(await engine.getTables(req.params?.databaseId));
-})
+});
 
-app.get('/api/connection/:connectionId/database/:databaseId/table/:tableId/columns', async (req, res) => {
-  const connection = await ConnectionUtils.getConnection(req.params?.connectionId);
-  const engine = new RelationalDatabaseEngine(connection.connection);
-  res.json(await engine.getColumns(req.params?.tableId, req.params?.databaseId));
-})
-
+app.get(
+  '/api/connection/:connectionId/database/:databaseId/table/:tableId/columns',
+  async (req, res) => {
+    const connection = await ConnectionUtils.getConnection(req.params?.connectionId);
+    const engine = new RelationalDatabaseEngine(connection.connection);
+    res.json(await engine.getColumns(req.params?.tableId, req.params?.databaseId));
+  },
+);
 
 app.post('/api/connection/:connectionId/execute', async (req, res) => {
   const connection = await ConnectionUtils.getConnection(req.params?.connectionId);
@@ -90,8 +92,8 @@ app.post('/api/connection/:connectionId/execute', async (req, res) => {
   // const sql = req?.inputs?.sql;
   // const database = req?.inputs?.database;
   // res.json(await engine.execute(sql, database));
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- preload.js
+++ preload.js
@@ -2,11 +2,11 @@
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
   for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+    replaceText(`${type}-version`, process.versions[type]);
   }
-})
+});
