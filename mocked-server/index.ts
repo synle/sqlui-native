a30526ae081c3beeebb0854ade09f43e00c73ad2
Sy Le <lenguyensy@gmail.com>
2022-01-24 12:58:47 -0800

    fixed issue where list tables failed because of npe for this.database

--- mocked-server/index.ts
+++ mocked-server/index.ts
@@ -17,7 +17,7 @@ let id = 0;
 
 const ConnectionUtils = {
   addConnection(connection: string, name: string): ConnectionProps {
-    const newId = `${++id}.${Date.now()}`;
+    const newId = `db.${++id}`;
 
     caches[newId] = {
       id: newId,
@@ -50,12 +50,11 @@ const ConnectionUtils = {
   },
 };
 
-// const engine = new RelationalDatabaseEngine('mssql://sa:password123!@localhost:1433');
-// ConnectionUtils.addConnection(`mysql://root:password@localhost:3306`, 'sqlui_test_mysql');
-// ConnectionUtils.addConnection(`mssql://sa:password123!@localhost:1433`, 'sqlui_test_mssql');
+ConnectionUtils.addConnection(`mysql://root:password@localhost:3306`, 'sqlui_test_mysql');
+ConnectionUtils.addConnection(`mssql://sa:password123!@localhost:1433`, 'sqlui_test_mssql');
 ConnectionUtils.addConnection(`mariadb://root:password@localhost:33061`, 'sqlui_test_mariadb');
-// ConnectionUtils.addConnection(`postgres://postgres:password@localhost:5432`, 'sqlui_test_postgres');
-// ConnectionUtils.addConnection(`sqlite://test.db`, 'sqlui_test_sqlite');
+ConnectionUtils.addConnection(`postgres://postgres:password@localhost:5432`, 'sqlui_test_postgres');
+ConnectionUtils.addConnection(`sqlite://test.db`, 'sqlui_test_sqlite');
 
 app.get('/api/connections', async (req, res) => {
   res.json(await ConnectionUtils.getConnections());
