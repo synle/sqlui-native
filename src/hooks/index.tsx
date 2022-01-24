ba912f750fad2a4a3b7f032ea6a6f0ca6e5068d4
Sy Le <lenguyensy@gmail.com>
2022-01-24 10:01:22 -0800

    hooking up react query

---
+++ src/hooks/index.tsx
@@ -0,0 +1,47 @@
+import {useQuery } from 'react-query'
+import ConnectionUtils from 'src/utils/ConnectionUtils';
+import { RelationalDatabaseEngine } from 'src/utils/RelationalDatabaseEngine';
+
+
+// useQuery('repoData', () =>
+//    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+//      res.json()
+//    )
+//  )
+
+
+export function useGetConnections(){
+  return useQuery(['connection'], () => ConnectionUtils.getConnections);
+}
+
+export function useGetConnection(connectionId: string){
+  return useQuery(['connection', connectionId], () => ConnectionUtils.getConnection(connectionId));
+}
+
+export function useAddConnection(){
+
+}
+
+export function useUpdateConnection(){
+
+}
+
+export function useDeleteConnection(){
+
+}
+
+export function useGetDatabases(engine: RelationalDatabaseEngine){
+  return engine.getDatabases();
+}
+
+export function useGetTables(engine: RelationalDatabaseEngine, database: string){
+  return engine.getTables(database);
+}
+
+export function useGetColumns(engine: RelationalDatabaseEngine, database: string){
+  return engine.getColumns(database);
+}
+
+export function useExecute(engine: RelationalDatabaseEngine, sql: string, database?: string){
+  return engine.execute(sql, database);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
