fd5c2506bfe502dba20641830957b90f2d97b730
Sy Le <lenguyensy@gmail.com>
2022-01-27 14:58:00 -0800

    streamline the edit connection page

--- src/views/NewConnectionPage/index.tsx
+++ src/views/NewConnectionPage/index.tsx
@@ -2,11 +2,8 @@ import { NewConnectionForm } from 'src/components/ConnectionForm';
 
 export default function NewConnectionPage() {
   return (
-    <div>
-      NewConnectionPage
-      <div>
-        <NewConnectionForm />
-      </div>
+    <div className='ConnectionPage'>
+      <NewConnectionForm />
     </div>
   );
 }
