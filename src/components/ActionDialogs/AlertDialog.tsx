fcb2d849d95906981c976d072683d144cb2d8d11
Sy Le <lenguyensy@gmail.com>
2022-01-28 08:53:52 -0800

    action dialogs (confirm, prompt, etc...)

---
+++ src/components/ActionDialogs/AlertDialog.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+interface AlertDialogProps {
+  open: boolean;
+  title: string;
+  message: string;
+  yesLabel?: string;
+  onYesClick: () => void;
+  noLabel?: string;
+  onNoClick: () => void;
+}
+
+export default function AlertDialog(props: AlertDialogProps) {
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        onClose={props.onNoClick}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'>
+        <DialogTitle id='alert-dialog-title'>{props.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>{props.message}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.onNoClick}>{props.noLabel || 'No'}</Button>
+          <Button onClick={props.onYesClick} autoFocus>
+            {props.yesLabel || 'Yes'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
