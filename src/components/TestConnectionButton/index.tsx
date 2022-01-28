a6aea0daa8e053cc40dfcc23e3ffa6ec93baed3d
Sy Le <lenguyensy@gmail.com>
2022-01-27 21:15:28 -0800

    added a toast component

--- src/components/TestConnectionButton/index.tsx
+++ src/components/TestConnectionButton/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
 import { useTestConnection } from 'src/hooks';
+import Toast from 'src/components/Toast';
 import { Sqlui } from 'typings';
 
 interface TestConnectionButtonProps {
@@ -9,21 +10,25 @@ interface TestConnectionButtonProps {
 }
 
 export default function TestConnectionButton(props: TestConnectionButtonProps) {
+  const [message, setMessage] = useState('');
   const { mutateAsync: testConnection } = useTestConnection();
 
   const onTestConnection = async () => {
     // const connectionMetadata = {};
     try {
       await testConnection(props.connection);
-      alert('good connection');
+      setMessage(`Successfully connected to Server`);
     } catch (err) {
-      alert('bad connection');
+      setMessage(`Failed to connect to Server`);
     }
   };
 
   return (
+    <>
     <Button type='button' onClick={() => onTestConnection()}>
       Test Connection
     </Button>
+    <Toast open={!!message} onClose={() => setMessage('')} message={message} />
+    </>
   );
 }
---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,30 @@
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+
+interface ToastProps {
+  open: boolean;
+  onClose: () => void;
+  message: string;
+}
+
+export default function Toast(props: ToastProps) {
+  const { message, open, onClose } = props;
+
+  const action = (
+    <>
+      <IconButton size='small' aria-label='close' color='inherit' onClick={onClose}>
+        <CloseIcon fontSize='small' />
+      </IconButton>
+    </>
+  );
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={onClose}
+      message={message}
+      action={action}
+    />
+  );
+}
