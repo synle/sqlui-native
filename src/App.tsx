bbabf1a02aeff0dab64e2a7a5d650ad7e639917d
Sy Le <lenguyensy@gmail.com>
2022-01-30 07:56:48 -0800

    Added shortcut keys for the app

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export default function App() {
           // then set it as current session
           setCurrentSessionId(newSession.id);
 
-          console.log('>> No Session Found, Getting you a new session', newSession)
+          console.log('>> No Session Found, Getting you a new session', newSession);
 
           // reload the page just in case
           // TODO: see if we need to use a separate row
