d290b8e482a2068d1c94a4e3d4d8c7c831ec76b9
Sy Le <lenguyensy@gmail.com>
2022-01-30 17:52:42 -0800

    minor hover state

--- src/components/Resizer/index.tsx
+++ src/components/Resizer/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import { grey } from '@mui/material/colors';
 
 // move this into a file
 interface ResizerProps {
@@ -11,5 +13,17 @@ export default function Resizer(props: ResizerProps) {
     props.onSetWidth(endX);
   };
 
-  return <div className='Resizer' draggable={true} onDragEnd={onDragEnd} />;
+  return (
+    <Box
+      draggable={true}
+      onDragEnd={onDragEnd}
+      sx={{
+        width: '15px',
+        cursor: 'col-resize',
+        '&:hover': {
+          background: grey[400],
+        },
+      }}
+    />
+  );
 }
