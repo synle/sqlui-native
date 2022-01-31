813aaed51ea7ada36e6a80281b868d7a6be0f65e
Sy Le <lenguyensy@gmail.com>
2022-01-30 22:15:04 -0800

    Fixed minor issue where the serialized fetch bridge is not properly formatted

--- commons/Endpoints.ts
+++ commons/Endpoints.ts
@@ -23,7 +23,7 @@ function addDataEndpoint(
       res.header('sqlui-native-session-id', req.headers['sqlui-native-session-id']);
       await incomingHandler(req, res, cache);
     } catch (err: any) {
-      res.status(500).send(err.stack || err.toString() || err);
+      res.status(500).send(err);
     }
   };
 
