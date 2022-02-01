3eaa990020cf71af79e391ad63ea6071c13414d5
Sy Le <lenguyensy@gmail.com>
2022-02-01 09:14:41 -0800

    Added pagination for DataTable (#29)
    * added pagination
    
    * 1.4.1

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,3 +1,4 @@
+import React, { useCallback } from 'react';
 import {
   useTable,
   usePagination,
@@ -12,6 +13,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
 import Paper from '@mui/material/Paper';
 
 interface DataTableProps {
@@ -19,46 +21,84 @@ interface DataTableProps {
   data: any[];
 }
 
+const pageSizeOptions : any[] = [10, 25, 50, 100, {label: 'Show All', value: -1}]
+
 export default function DataTable(props: DataTableProps) {
   const { columns, data } = props;
-  const tableInstance = useTable({ columns, data });
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+
+  //@ts-ignore
+  const {
+    getTableBodyProps,
+    gotoPage,
+    headerGroups,
+    page,
+    prepareRow,
+    setPageSize,
+    state,
+  } = useTable(
+    {
+      initialState: {
+        pageSize: 50,
+      },
+      columns,
+      data,
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination,
+  );
+  const { pageIndex, pageSize } = state;
+
+  const onPageChange = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement> | null, page: number) => {
+      gotoPage(page);
+    },
+    [],
+  );
+
+  const onRowsPerPageChange = useCallback((e) => {
+    setPageSize(e.target.value);
+  }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size='small'>
-        <TableHead>
-          {
-            headerGroups.map((headerGroup) => (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size='small'>
+          <TableHead>
+            {headerGroups.map((headerGroup) => (
               <TableRow {...headerGroup.getHeaderGroupProps()}>
-                {
-                  headerGroup.headers.map((column) => (
-                    <TableCell {...column.getHeaderProps()}>
-                      {column.render('Header')}
-                    </TableCell>
-                  ))
-                }
+                {headerGroup.headers.map((column) => (
+                  <TableCell {...column.getHeaderProps()}>{column.render('Header')}</TableCell>
+                ))}
               </TableRow>
-            ))
-          }
-        </TableHead>
-        <TableBody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <TableRow {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <TableCell>
-                      {cell.render('Cell')}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            ))}
+          </TableHead>
+          <TableBody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <TableRow {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return <TableCell>{cell.render('Cell')}</TableCell>;
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        component='div'
+        count={data.length}
+        onPageChange={onPageChange}
+        onRowsPerPageChange={onRowsPerPageChange}
+        page={pageIndex}
+        rowsPerPage={pageSize}
+        rowsPerPageOptions={pageSizeOptions}
+        showFirstButton
+        showLastButton
+      />
+    </>
   );
 }
