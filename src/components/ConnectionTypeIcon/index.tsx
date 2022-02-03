6fd1208aff455d4ff4452cfd5948882249a5ccc6
Sy Le <lenguyensy@gmail.com>
2022-02-03 12:22:28 -0800

    Revamped show hints (#51)
    * revamped the connection hints
    
    * update icon
    
    * update the link to be more useful
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

---
+++ src/components/ConnectionTypeIcon/index.tsx
@@ -0,0 +1,38 @@
+import CloudIcon from '@mui/icons-material/Cloud';
+import { SqluiCore } from 'typings';
+
+interface ConnectionTypeIconProps {
+  scheme?: string;
+  status?: string;
+}
+
+export default function ConnectionTypeIcon(props: ConnectionTypeIconProps) {
+  const { scheme, status } = props;
+
+  if (status !== 'online') {
+    return <CloudIcon color='disabled' fontSize='large' />;
+  }
+
+  switch (scheme) {
+    case 'mssql':
+      return (
+        <img src={`${process.env.PUBLIC_URL}/assets/sqlserver.png`} title={scheme} width={30} />
+      );
+    case 'postgres':
+      return (
+        <img src={`${process.env.PUBLIC_URL}/assets/postgresql.png`} title={scheme} width={30} />
+      );
+    case 'sqlite':
+      return <img src={`${process.env.PUBLIC_URL}/assets/sqlite.png`} title={scheme} width={30} />;
+    case 'mariadb':
+      return <img src={`${process.env.PUBLIC_URL}/assets/mariadb.png`} title={scheme} width={30} />;
+    case 'mysql':
+      return <img src={`${process.env.PUBLIC_URL}/assets/mysql.png`} title={scheme} width={30} />;
+    case 'cassandra':
+      return (
+        <img src={`${process.env.PUBLIC_URL}/assets/cassandra.png`} title={scheme} width={30} />
+      );
+    default:
+      return <CloudIcon color='primary' fontSize='large' />;
+  }
+}
