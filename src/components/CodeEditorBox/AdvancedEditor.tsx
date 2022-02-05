4cfc3364c334b7a1dd49614f9a98c4451b66e236
Sy Le <lenguyensy@gmail.com>
2022-02-04 19:51:44 -0800

    Uses a more advanced editor for better code highlighting  (#62)
    * added monaco code editor and remove old code editor component
    
    * cleaned up
    
    * added the component for code editor
    
    * now uses monaco editor
    
    * 1.9.0
    
    * make monaco code editor respecting color scheme from dark mode
    
    * selectively picked up the correct editor to use
    
    * going back to the pure monaco
    
    * minor update to prevent default for command
    
    * now uses the core monaco editor
    
    * added the wrap option
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

---
+++ src/components/CodeEditorBox/AdvancedEditor.tsx
@@ -0,0 +1,75 @@
+import { useRef, useState, useEffect, useCallback } from 'react';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { styled, createTheme, ThemeProvider } from '@mui/system';
+import { useDarkModeSetting } from 'src/hooks';
+
+type AdvancedEditorProps = {
+  language?: 'sql' | string;
+  value?: string;
+  onChange?: (newValue: string) => void;
+  onBlur?: (newValue: string) => void;
+  wordWrap?: boolean;
+};
+
+const AdvancedEditorContainer = styled('div')(({ theme }) => {
+  return {
+    height: '300px',
+    width: '100%',
+  };
+});
+
+const DEFAULT_OPTIONS = {
+  // lineNumbers: 'off',
+  glyphMargin: false,
+  folding: false,
+  minimap: {
+    enabled: false,
+  },
+};
+
+export default function AdvancedEditor(props: AdvancedEditorProps) {
+  const colorMode = useDarkModeSetting();
+  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const monacoEl = useRef(null);
+
+  // this is used to clean up the editor
+  useEffect(() => {
+    return () => {
+      editor?.dispose();
+      setEditor(null);
+    };
+  }, []);
+
+  // we used this block to set the value of the editor
+  useEffect(() => {
+    if (editor) {
+      editor.setValue(props.value || '');
+    }
+  }, [editor, props.value]);
+
+  // here we will initiate the editor
+  // and can be also be used to update the settings
+  useEffect(() => {
+    editor?.dispose();
+
+    if (monacoEl.current) {
+      const newEditor = monaco.editor.create(monacoEl.current!, {
+        value: props.value,
+        language: props.language,
+        minimap: {
+          enabled: false,
+        },
+        theme: colorMode === 'dark' ? 'vs-dark' : 'light',
+        wordWrap: props.wordWrap === true ? 'on' : 'off',
+      });
+
+      newEditor.onDidBlurEditorWidget(() => {
+        props.onBlur && props.onBlur(newEditor.getValue() || '');
+      });
+
+      setEditor(newEditor);
+    }
+  }, [monacoEl, props.wordWrap, colorMode]);
+
+  return <AdvancedEditorContainer ref={monacoEl}></AdvancedEditorContainer>;
+}
