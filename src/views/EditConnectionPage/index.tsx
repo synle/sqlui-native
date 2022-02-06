72b94db281c210c04ab4452931cfae2345bb0b98
Sy Le <lenguyensy@gmail.com>
2022-02-05 20:40:25 -0800

    Persist sidebar width (#72)
    * Persist sidebar width
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/views/EditConnectionPage/index.tsx
+++ src/views/EditConnectionPage/index.tsx
@@ -6,11 +6,18 @@ import ConnectionDescription from 'src/components/ConnectionDescription';
 import Resizer from 'src/components/Resizer';
 import { useParams } from 'react-router-dom';
 import { EditConnectionForm } from 'src/components/ConnectionForm';
+import { LocalStorageConfig } from 'src/data/config';
 
 export default function EditConnectionPage() {
-  const [width, setWidth] = useState<undefined | number>();
   const urlParams = useParams();
   const connectionId = urlParams.connectionId as string;
+  const [width, setWidth] = useState<undefined | number>(
+    LocalStorageConfig.get<number>('clientConfig/leftPanelWidth', 300),
+  );
+  const onSetWidth = (newWidth: number) => {
+    LocalStorageConfig.set('clientConfig/leftPanelWidth', newWidth);
+    setWidth(newWidth);
+  };
 
   if (!connectionId) {
     return null;
@@ -22,7 +29,7 @@ export default function EditConnectionPage() {
         <NewConnectionButton />
         <ConnectionDescription />
       </div>
-      <Resizer onSetWidth={setWidth} />
+      <Resizer onSetWidth={onSetWidth} />
       <div className='LayoutTwoColumns__RightPane'>
         <Typography variant='h5' gutterBottom={true} sx={{ mt: 1 }}>
           Edit Connection
--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -3,9 +3,16 @@ import NewConnectionButton from 'src/components/NewConnectionButton';
 import ConnectionDescription from 'src/components/ConnectionDescription';
 import QueryBoxTabs from 'src/components/QueryBoxTabs';
 import Resizer from 'src/components/Resizer';
+import { LocalStorageConfig } from 'src/data/config';
 
 export default function MainPage() {
-  const [width, setWidth] = useState<undefined | number>();
+  const [width, setWidth] = useState<undefined | number>(
+    LocalStorageConfig.get<number>('clientConfig/leftPanelWidth', 300),
+  );
+  const onSetWidth = (newWidth: number) => {
+    LocalStorageConfig.set('clientConfig/leftPanelWidth', newWidth);
+    setWidth(newWidth);
+  };
 
   return (
     <section className='MainPage LayoutTwoColumns'>
@@ -13,7 +20,7 @@ export default function MainPage() {
         <NewConnectionButton />
         <ConnectionDescription />
       </div>
-      <Resizer onSetWidth={setWidth} />
+      <Resizer onSetWidth={onSetWidth} />
       <div className='LayoutTwoColumns__RightPane'>
         <QueryBoxTabs />
       </div>
