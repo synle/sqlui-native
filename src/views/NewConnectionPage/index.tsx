c52ac2fa7ca037ef9e920c69d087f372741e78a8
Sy Le <lenguyensy@gmail.com>
2022-02-05 20:28:30 -0800

    Persist sidebar width

--- src/views/NewConnectionPage/index.tsx
+++ src/views/NewConnectionPage/index.tsx
@@ -4,9 +4,14 @@ import { NewConnectionForm } from 'src/components/ConnectionForm';
 import NewConnectionButton from 'src/components/NewConnectionButton';
 import ConnectionDescription from 'src/components/ConnectionDescription';
 import Resizer from 'src/components/Resizer';
+import { LocalStorageConfig } from 'src/data/config';
 
 export default function NewConnectionPage() {
-  const [width, setWidth] = useState<undefined | number>();
+  const [width, setWidth] = useState<undefined | number>(LocalStorageConfig.get<number>('clientConfig/leftPanelWidth', 300));
+  const onSetWidth = (newWidth: number) => {
+    LocalStorageConfig.set('clientConfig/leftPanelWidth', newWidth);
+    setWidth(newWidth);
+  }
 
   return (
     <section className='NewConnectionPage LayoutTwoColumns'>
@@ -14,7 +19,7 @@ export default function NewConnectionPage() {
         <NewConnectionButton />
         <ConnectionDescription />
       </div>
-      <Resizer onSetWidth={setWidth} />
+      <Resizer onSetWidth={onSetWidth} />
       <div className='LayoutTwoColumns__RightPane'>
         <Typography variant='h5' gutterBottom={true} sx={{ mt: 1 }}>
           New Connection
