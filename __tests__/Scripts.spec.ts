df0746c33bca85a5e0c8198b11cdc1a9927b8377
Sy Le <lenguyensy@gmail.com>
2022-02-10 14:05:34 -0800

    Generate the document guide for query help... (#112)
    * generate the document guide
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- __tests__/Scripts.spec.ts
+++ __tests__/Scripts.spec.ts
@@ -1,4 +1,7 @@
+import fs from 'fs';
 import { SqluiCore, SqlAction } from 'typings';
+import { formatJS } from 'src/utils/formatter';
+import { formatSQL } from 'src/utils/formatter';
 import { scripts as RmdbScripts } from 'src/scripts/rmdb';
 import { scripts as CassandraScripts } from 'src/scripts/cassandra';
 import { scripts as MongodbScripts } from 'src/scripts/mongodb';
@@ -29,39 +32,85 @@ function _getScript(scripts: SqlAction.ScriptGenerator[], dialect: SqluiCore.Dia
 }
 
 describe('Scripts', () => {
+  let commandGuides: string[] = [];
+
+  function addGuideText(sectionName: string, scripts: (SqlAction.Output | undefined)[]) {
+    commandGuides.push(`## ${sectionName}\n`);
+
+    for (const script of scripts) {
+      if (script && script.query) {
+        commandGuides.push(`### ${script.label}\n`);
+
+        let query = script.query
+          .replace(/--/g, '\n')
+          .replace(/\n/g, ' ')
+          .replace(/[ ][ ]+/g, ' ')
+          .trim();
+        switch (script.formatter) {
+          case 'sql':
+            query = formatSQL(query);
+          case 'js':
+            query = formatJS(query);
+            break;
+        }
+
+        commandGuides.push('```');
+        commandGuides.push(query);
+        commandGuides.push('```\n\n');
+      }
+    }
+  }
+
   test('RmdbScripts - mysql', async () => {
     const scripts = _getScript(RmdbScripts, 'mysql');
     expect(scripts).toMatchSnapshot();
+    addGuideText('mysql', scripts);
   });
+
   test('RmdbScripts - mariadb', async () => {
     const scripts = _getScript(RmdbScripts, 'mariadb');
     expect(scripts).toMatchSnapshot();
+    addGuideText('mariadb', scripts);
   });
+
   test('RmdbScripts - mssql', async () => {
     const scripts = _getScript(RmdbScripts, 'mssql');
     expect(scripts).toMatchSnapshot();
+    addGuideText('mssql', scripts);
   });
+
   test('RmdbScripts - postgres', async () => {
     const scripts = _getScript(RmdbScripts, 'postgres');
     expect(scripts).toMatchSnapshot();
+    addGuideText('postgres', scripts);
   });
+
   test('RmdbScripts - sqlite', async () => {
     const scripts = _getScript(RmdbScripts, 'sqlite');
     expect(scripts).toMatchSnapshot();
+    addGuideText('sqlite', scripts);
   });
 
   test('CassandraScripts', async () => {
     const scripts = _getScript(CassandraScripts, 'cassandra');
     expect(scripts).toMatchSnapshot();
+    addGuideText('cassandra', scripts);
   });
 
   test('MongodbScripts', async () => {
     const scripts = _getScript(MongodbScripts, 'mongodb');
     expect(scripts).toMatchSnapshot();
+    addGuideText('mongodb', scripts);
   });
 
   test('RedisScripts', async () => {
     const scripts = _getScript(RedisScripts, 'redis');
     expect(scripts).toMatchSnapshot();
+    addGuideText('redis', scripts);
+  });
+
+  test('Consolidate the guide into a command', async () => {
+    const newGuide = commandGuides.join('\n');
+    fs.writeFileSync('./guides.md', newGuide);
   });
 });
