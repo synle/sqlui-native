e07183737818b0ad5b75243428cf9ccbcd109919
Sy Le <lenguyensy@gmail.com>
2022-02-13 15:17:15 -0800

    Added expandConnections and a few more connection queries in command palette (#164)
    * more actions
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/components/MissionControl/index.tsx
+++ src/components/MissionControl/index.tsx
@@ -386,11 +386,13 @@ export default function MissionControl() {
       });
 
       createSystemNotification(
-        `Connection "${connection.name}" (dialect=${connection.dialect}) deleted`,
+        `Connection "${connection.name}" (dialect=${connection.dialect || 'N/A'}) deleted`,
       );
     } catch (err) {
       curToast = await addToast({
-        message: `Failed to delete connection "${connection.name}" (dialect=${connection.dialect})`,
+        message: `Failed to delete connection "${connection.name}" (dialect=${
+          connection.dialect || 'N/A'
+        })`,
       });
     }
   };
@@ -405,7 +407,9 @@ export default function MissionControl() {
     let resultMessage = '';
     try {
       await reconnectConnection(connection.id);
-      resultMessage = `Successfully connected to "${connection.name}" (dialect=${connection.dialect})`;
+      resultMessage = `Successfully connected to "${connection.name}" (dialect=${
+        connection.dialect || 'N/A'
+      })`;
     } catch (err) {
       resultMessage = `Failed to connect to "${connection.name}"`;
     }
