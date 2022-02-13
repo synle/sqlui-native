12ffb2d4913b552732a8ae1cbb16f51c34228d19
Sy Le <lenguyensy@gmail.com>
2022-02-13 07:08:28 -0800

    Uses gap for flex so we don't need inline margin left and fixed tab re-ordering cache issue (#157)
    * now uses gap for flex so we don't need inline margin left
    
    * fixed a minor issue where tab rearrange not working because the cache is not properly refreshed
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/components/QueryBox/index.tsx
+++ src/components/QueryBox/index.tsx
@@ -131,16 +131,14 @@ export default function QueryBox(props: QueryBoxProps) {
           <ConnectionDatabaseSelector value={query} onChange={onDatabaseConnectionChange} />
           <ConnectionRevealButton query={query} />
         </div>
-        <div className='FormInput__Row'>
-          <CodeEditorBox
-            value={query.sql}
-            placeholder={`Enter SQL for ` + query.name}
-            onChange={onSqlQueryChange}
-            language={language}
-            autoFocus
-            mode='textarea'
-          />
-        </div>
+        <CodeEditorBox
+          value={query.sql}
+          placeholder={`Enter SQL for ` + query.name}
+          onChange={onSqlQueryChange}
+          language={language}
+          autoFocus
+          mode='textarea'
+        />
         <div className='FormInput__Row'>
           <Button
             type='submit'
@@ -155,8 +153,7 @@ export default function QueryBox(props: QueryBoxProps) {
               type='button'
               variant='outlined'
               onClick={() => selectCommand({ event: 'clientEvent/showQueryHelp' })}
-              startIcon={<HelpIcon />}
-              sx={{ ml: 3 }}>
+              startIcon={<HelpIcon />}>
               Show Query Help
             </Button>
           </Tooltip>
@@ -166,14 +163,13 @@ export default function QueryBox(props: QueryBoxProps) {
               type='button'
               variant='outlined'
               onClick={onFormatQuery}
-              startIcon={<FormatColorTextIcon />}
-              sx={{ ml: 3 }}>
+              startIcon={<FormatColorTextIcon />}>
               Format
             </Button>
           </Tooltip>
         </div>
+        <ResultBox query={query} executing={executing} />
       </form>
-      <ResultBox query={query} executing={executing} />
     </>
   );
 }
