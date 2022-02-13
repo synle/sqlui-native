c93468b559c60ef021364429e88d8f0798eda07b
Sy Le <lenguyensy@gmail.com>
2022-02-12 18:42:38 -0800

    Added more unit tests for ordering change and exported queries (#151)
    * extracted the ordering function
    
    * added unit test for ordering
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/hooks/hooks.spec.ts
+++ src/hooks/hooks.spec.ts
@@ -2,7 +2,7 @@ import * as hooks from 'src/hooks';
 
 describe('hooks', () => {
   describe('getExportedConnection', () => {
-    test('should work', async () => {
+    test('should work with minimal inputs', async () => {
       const actual = hooks.getExportedConnection({
         id: 'connection.1643485467072.6333713976068809',
         connection: 'mysql://root:password@localhost:3306',
@@ -10,12 +10,17 @@ describe('hooks', () => {
         status: 'online',
         dialect: 'mysql',
       });
-      expect(actual).toMatchSnapshot();
+      expect(actual).toStrictEqual({
+        _type: 'connection',
+        connection: 'mysql://root:password@localhost:3306',
+        id: 'connection.1643485467072.6333713976068809',
+        name: 'sy mysql 123',
+      });
     });
   });
 
   describe('getExportedQuery', () => {
-    test('should work', async () => {
+    test('should work with minimal inputs', async () => {
       const actual = hooks.getExportedQuery({
         id: 'query.1643737746323.6184509846791006',
         name: 'Query 2/1/2022, 9:49:06 AM',
@@ -24,6 +29,63 @@ describe('hooks', () => {
         databaseId: 'musicstores',
       });
       expect(actual).toMatchSnapshot();
+    });
+
+    test('should work with more completed data inputs', async () => {
+      //@ts-ignore
+      const actual = hooks.getExportedQuery({
+        id: 'query.1643737746323.6184509846791006',
+        name: 'Query 2/1/2022, 9:49:06 AM',
+        sql: 'SELECT\n  TOP 200 *\nFROM\n  albums',
+        connectionId: 'connection.1644098335891.8887562323718656',
+        selected: true,
+        databaseId: 'musicstores',
+        executionStart: 123,
+        executionEnd: 456,
+        result: {
+          ok: true,
+          raw: [{ aa: 777 }],
+        },
+      });
+      expect(actual).toMatchSnapshot();
+    });
+  });
+
+  describe('getUpdatedOrdersForList', () => {
+    const items = [11, 22, 33, 44, 55];
+    test('should work for from=4, to=2', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 4, 2);
+      expect(actual).toStrictEqual([11, 22, 55, 33, 44]);
+    });
+
+    test('should work for from=4, to=3', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 4, 3);
+      expect(actual).toStrictEqual([11, 22, 33, 55, 44]);
+    });
+
+    test('should work for from=4, to=0', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 4, 0);
+      expect(actual).toStrictEqual([55, 11, 22, 33, 44]);
+    });
+
+    test('should work for from=0, to=1', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 0, 1);
+      expect(actual).toStrictEqual([22, 11, 33, 44, 55]);
+    });
+
+    test('should work for from=0, to=4', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 0, 4);
+      expect(actual).toStrictEqual([22, 33, 44, 55, 11]);
+    });
+
+    test('should work for from=0, to=3', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 0, 3);
+      expect(actual).toStrictEqual([22, 33, 44, 11, 55]);
+    });
+
+    test('should work for from=1, to=3', async () => {
+      let actual = hooks.getUpdatedOrdersForList(items, 1, 3);
+      expect(actual).toStrictEqual([11, 33, 44, 22, 55]);
     });
   });
 });
