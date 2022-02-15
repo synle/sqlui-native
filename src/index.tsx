4ec3d8e4127549fde77a554fa9806f8b00952028
Sy Le <lenguyensy@gmail.com>
2022-02-15 14:08:47 -0800

    Make monaco editor highilghts working on the native electron

--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,24 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import React from 'react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
+window.addEventListener('sqluiNativeEvent/ready', function () {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
     },
-  },
-});
+  });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <ReactQueryDevtools initialIsOpen={false} />
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.querySelector('#body'),
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>,
+    document.querySelector('#body'),
+  );
+}, false);
