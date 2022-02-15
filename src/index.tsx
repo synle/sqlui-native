5407e8789385c2d9343675759b17f3fe94a8ed76
synle <synle@users.noreply.github.com>
2022-02-15 23:38:26 +0000

    CI / CD - Prettier Automatic Commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,27 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import React from 'react';
 import App from './App';
 
-window.addEventListener('sqluiNativeEvent/ready', function () {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
+window.addEventListener(
+  'sqluiNativeEvent/ready',
+  function () {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
       },
-    },
-  });
+    });
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <CssBaseline />
-        <ReactQueryDevtools initialIsOpen={false} />
-        <App />
-      </QueryClientProvider>
-    </React.StrictMode>,
-    document.querySelector('#body'),
-  );
-}, false);
+    ReactDOM.render(
+      <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <App />
+        </QueryClientProvider>
+      </React.StrictMode>,
+      document.querySelector('#body'),
+    );
+  },
+  false,
+);
