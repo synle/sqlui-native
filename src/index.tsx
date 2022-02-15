256bdfacd9041d3bd5c8ce114530c94af22d2ab0
Sy Le <lenguyensy@gmail.com>
2022-02-15 15:39:44 -0800

    Finally getting monaco syntax highlights to work in an electron app and the mocked server (#184)
    * Make monaco editor highilghts working on the native electron
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,28 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import React from 'react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
+window.addEventListener(
+  'sqluiNativeEvent/ready',
+  function () {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <App />
+        </QueryClientProvider>
+      </React.StrictMode>,
+      document.querySelector('#body'),
+    );
   },
-});
-
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <ReactQueryDevtools initialIsOpen={false} />
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.querySelector('#body'),
+  false,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
