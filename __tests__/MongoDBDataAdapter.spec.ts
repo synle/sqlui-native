c0a015300adb26fb080b4f87c6ea69f0cd531e91
Sy Le <lenguyensy@gmail.com>
2022-02-16 19:58:57 -0800

    wip 3d

---
+++ __tests__/MongoDBDataAdapter.spec.ts
@@ -0,0 +1,30 @@
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    if(db.close) {
+      await db.close();
+    }
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import MainPage from 'src/views/MainPage';
 import NewConnectionPage from 'src/views/NewConnectionPage';
 import './App.scss';
 import 'src/electronRenderer';
+import * as THREE from 'three';
 
 export default function App() {
   const [hasValidSessionId, setHasValidSessionId] = useState(false);
@@ -30,122 +31,135 @@ export default function App() {
   const { add: addToast } = useToaster();
   const toasterRef = useRef<ToasterHandler | undefined>();
 
-  const myTheme = createTheme({
-    // Theme settings
-    palette: {
-      mode: colorMode,
-    },
-  });
-
   useEffect(() => {
-    async function _validateSession() {
-      if (sessions) {
-        if (sessions.length === 0) {
-          // if there is no session, let's create the session
-          const newSession = await upsertSession({
-            id: getDefaultSessionId(),
-            name: `Default Session`,
-          });
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-          // then set it as current session
-          setCurrentSessionId(newSession.id);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-          console.log('>> No Session Found, Getting you a new session', newSession);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const geometry = new THREE.BoxGeometry();
+    // const cube = new THREE.Mesh( geometry, material );
+    // scene.add(new THREE.Mesh( geometry, material ) );
 
-          // reload the page just in case
-          // TODO: see if we need to use a separate row
-          window.location.reload();
-        } else {
-          // TODO: see if we need to check again the
-          // current sessionId
-          // assume we don't do delete session
-          // this will fine, we don't need to check if the sessionId
-          // is present in the list of available sessions
-          setHasValidSessionId(true);
-        }
+
+    var basicMaterial = new THREE.MeshLambertMaterial({
+        color: 0x0095DD
+    });
+
+    // scene.add(new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial( {color: 0x00ff00} ) ) );
+    for(let i = 1; i <= 3; i++){
+      const mesh = new THREE.Mesh( new THREE.BoxGeometry(5, 5, 5), new THREE.MeshLambertMaterial({
+          color: [0xff0000, 0x00ff00, 0x0000ff][i % 3]
+      }) )
+
+      mesh.position.set(0,0, i * 5)
+      scene.add(mesh);
+    }
+
+    // camera.position.x = 0;
+    // camera.position.y = -15;
+    // camera.position.z = 10;
+
+    // camera.rotation.x = 1.5
+
+    camera.position.y = -15;
+    camera.lookAt(new THREE.Vector3( 0, 0, 5 ));
+
+    function animate() {
+      requestAnimationFrame( animate );
+
+      // cube.rotation.x += 0.01;
+      // cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    };
+
+    // const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    // scene.add( light );
+
+
+        // var light = new THREE.AmbientLight(0x404040);
+
+    // var light = new THREE.AmbientLight(0xf6e86d);
+    // scene.add(light);
+
+    // var light = new THREE.DirectionalLight(0x404040, 0.5);
+    // scene.add(light);
+
+    var light = new THREE.HemisphereLight(0x404040, 0xFFFFFF, 0.5);
+    scene.add(light);
+
+
+
+    // animate();
+
+    document.addEventListener('keydown', (e: KeyboardEvent) => {
+      const key = e.key;
+
+      const delta = 0.1;
+
+      switch(key){
+        case 'w':
+          camera.position.y += delta;
+          break;
+        case 's':
+          camera.position.y -= delta;
+          break;
+        case 'a':
+          camera.position.x -= delta;
+          break;
+        case 'd':
+          camera.position.x += delta;
+          break;
+        case 'q':
+          camera.position.z -= delta;
+          break;
+        case 'e':
+          camera.position.z += delta;
+          break;
+
+        case 'i':
+          camera.rotation.x += delta;
+          camera.lookAt(new THREE.Vector3( 0, 0, 5 ));
+          break;
+        case 'k':
+          camera.rotation.x -= delta;
+          break;
+        case 'j':
+          camera.rotation.y += delta;
+          break;
+        case 'l':
+          camera.rotation.y -= delta;
+          break;
+        case 'u':
+          camera.rotation.z += delta;
+          break;
+        case 'o':
+          camera.rotation.z -= delta;
+          break;
       }
-    }
 
-    _validateSession();
-  }, [sessions]);
+      renderer.render( scene, camera );
 
-  const isLoading = loadingSessions || loadingCurrentSession;
+      console.log(camera.position, camera.rotation)
+    });
 
-  if (isLoading) {
-    return null;
-  }
+    renderer.render( scene, camera );
 
-  if (!hasValidSessionId) {
-    return (
-      <Alert severity='info' icon={<CircularProgress size={15} />}>
-        Loading sqlui-native, please wait...
-      </Alert>
-    );
-  }
-
-  const onDrop = async (e: React.DragEvent) => {
-    if (e.dataTransfer.items && e.dataTransfer.items.length === 1) {
-      e.preventDefault();
-
-      await toasterRef.current?.dismiss();
-
-      toasterRef.current = await addToast({
-        message: `Parsing the file for importing, please wait...`,
-      });
-
-      // TODO: right now only support one file for drop...
-      const files = [...e.dataTransfer.items]
-        .map((item) => item.getAsFile())
-        .filter((f) => f) as File[];
-
-      //@ts-ignore
-      const file = files[0];
-      const data = await dataApi.readFileContent(file);
-
-      await toasterRef.current?.dismiss();
-      toasterRef.current = undefined;
-      selectCommand({ event: 'clientEvent/import', data });
-    }
-  };
-
-  const onDragOver = async (e: React.DragEvent) => {
-    if (e.dataTransfer.items && e.dataTransfer.items.length === 1) {
-      e.preventDefault();
-
-      if (!toasterRef.current) {
-        toasterRef.current = await addToast({
-          message: `Drop the file here to upload and import it.`,
-        });
-      }
-    }
-  };
+    console.log(camera.position, camera.rotation)
+  },[])
 
   return (
-    <ThemeProvider theme={myTheme}>
+    <>
       <HashRouter>
-        <Box
-          className='App'
-          sx={{
-            bgcolor: 'background.default',
-            color: 'text.primary',
-          }}
-          onDrop={onDrop}
-          onDragOver={onDragOver}>
-          <AppHeader />
-          <section className='App__Section'>
-            <Routes>
-              <Route path='/' element={<MainPage />} />
-              <Route path='/connection/new' element={<NewConnectionPage />} />
-              <Route path='/connection/edit/:connectionId' element={<EditConnectionPage />} />
-              <Route path='/*' element={<MainPage />} />
-            </Routes>
-          </section>
-        </Box>
         <MissionControl />
       </HashRouter>
       <ActionDialogs />
       <Toasters />
       <ElectronEventListener />
-    </ThemeProvider>
+    </>
   );
 }
