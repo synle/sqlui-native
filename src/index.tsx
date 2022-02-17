dd1588d1c662bed9baa7cb5458e0a9dbdd72716a
Sy Le <syle@linkedin.com>
2022-02-16 16:42:36 -0800

    now use event instead of random init for syncing

--- src/index.tsx
+++ src/index.tsx
@@ -5,27 +5,35 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import React from 'react';
 import App from './App';
 
+
+const renderApp = function () {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>,
+    document.querySelector('#body'),
+  );
+
+  window.removeEventListener('sqluiNativeEvent/ready', renderApp);
+}
+
 window.addEventListener(
   'sqluiNativeEvent/ready',
-  function () {
-    const queryClient = new QueryClient({
-      defaultOptions: {
-        queries: {
-          retry: false,
-        },
-      },
-    });
-
-    ReactDOM.render(
-      <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <CssBaseline />
-          <ReactQueryDevtools initialIsOpen={false} />
-          <App />
-        </QueryClientProvider>
-      </React.StrictMode>,
-      document.querySelector('#body'),
-    );
-  },
+  renderApp,
   false,
 );
+
+// tell the main app to get ready for initiation
+window.dispatchEvent(new Event('sqluiNativeEvent/init'));
