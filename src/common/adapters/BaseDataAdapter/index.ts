537b7a8aaf7a264079db36282159ed4c1768dba9
Sy Le <lenguyensy@gmail.com>
2022-05-11 14:54:10 -0700

    clean ups (#320)

--- src/common/adapters/BaseDataAdapter/index.ts
+++ src/common/adapters/BaseDataAdapter/index.ts
@@ -39,7 +39,6 @@ export default abstract class BaseDataAdapter {
       case 'sqlite':
       case 'cassandra':
       case 'mongodb':
-      case 'cosmosdb':
         if (dialect) {
           const connectionStringParser = new ConnectionStringParser({
             scheme: dialect,
