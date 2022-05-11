42f077bab6fc095f8a0022d2bffec5f86dfdd0de
Sy Le <lenguyensy@gmail.com>
2022-05-11 13:56:59 -0700

    Refactoring - moving script adapters into the common folder (#319)
    * minor clean up and refactoring
    
    * move sql into the common
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/components/TableActions/index.tsx
+++ src/frontend/components/TableActions/index.tsx
@@ -1,9 +1,9 @@
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import IconButton from '@mui/material/IconButton';
 import { useState } from 'react';
+import { getTableActions } from 'src/common/adapters/DataScriptFactory';
 import DropdownButton from 'src/frontend/components/DropdownButton';
 import { useCommands } from 'src/frontend/components/MissionControl';
-import { getTableActions } from 'src/frontend/data/sql';
 import { useGetColumns, useGetConnectionById } from 'src/frontend/hooks/useConnection';
 import {
   getIsTableIdRequiredForQuery,
