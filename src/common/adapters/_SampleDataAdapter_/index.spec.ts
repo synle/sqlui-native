bc023ca568ef00d7d013128e702631e9705e9d60
Sy Le <lenguyensy@gmail.com>
2022-05-11 14:32:57 -0700

    clean up readme

--- src/common/adapters/_SampleDataAdapter_/index.spec.ts
+++ src/common/adapters/_SampleDataAdapter_/index.spec.ts
@@ -1,9 +1,9 @@
-import YOUR_ADAPTER_NAME from 'src/common/adapters/YOUR_ADAPTER_NAME/index';
+// import YOUR_ADAPTER_NAME from 'src/common/adapters/YOUR_ADAPTER_NAME/index';
 
-const adapter = new YOUR_ADAPTER_NAME('some_connection');
+// const adapter = new YOUR_ADAPTER_NAME('some_connection');
 
-describe.skip('redis', () => {
-  test('Set', async () => {
-    await adapter.execute(`db.set('key', 'value123');`);
+describe.skip('YOUR_ADAPTER_NAME', () => {
+  test('Sample test', async () => {
+    expect(1).toBe(1);
   });
 });
