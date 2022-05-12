eaf8271d5a6bad3b95ede8c36b6f9db5521d3a6f
Sy Le <lenguyensy@gmail.com>
2022-05-12 09:38:05 -0700

    connection actions

--- typings/index.ts
+++ typings/index.ts
@@ -154,9 +154,12 @@ export module SqluiFrontend {
 }
 
 export module SqlAction {
-  export type CoreInput = {
+  export type ConnectionInput = {
     dialect?: string;
     connectionId?: string;
+  }
+
+  export type CoreInput = ConnectionInput & {
     databaseId?: string;
     querySize: number;
   };
