b66944731d187a6c764e60d3f3b7b8a55253126d
Sy Le <lenguyensy@gmail.com>
2022-06-04 11:45:40 -0700

    Now hide context menu for sidebar in modes where it isn't meaningful (#374)
    * callback to select stuffs
    
    * 1.32.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/components/ConnectionActions/index.tsx
+++ src/frontend/components/ConnectionActions/index.tsx
@@ -11,6 +11,7 @@ import { useNavigate } from 'react-router-dom';
 import { getConnectionActions } from 'src/common/adapters/DataScriptFactory';
 import DropdownButton from 'src/frontend/components/DropdownButton';
 import { useCommands } from 'src/frontend/components/MissionControl';
+import { useTreeActions } from 'src/frontend/hooks/useTreeActions';
 import { SqluiCore } from 'typings';
 
 type ConnectionActionsProps = {
@@ -22,6 +23,7 @@ export default function ConnectionActions(props: ConnectionActionsProps) {
   const navigate = useNavigate();
   const { selectCommand } = useCommands();
   const data = connection;
+  const { data: treeActions } = useTreeActions();
 
   const { dialect, id: connectionId } = connection;
 
@@ -95,6 +97,10 @@ export default function ConnectionActions(props: ConnectionActionsProps) {
         }),
     })),
   ];
+
+  if (!treeActions.showContextMenu) {
+    return null;
+  }
 
   return (
     <>
