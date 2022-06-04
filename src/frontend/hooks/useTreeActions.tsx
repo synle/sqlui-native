c12d85f281b3ad6da2fcd039bffd5f71fb9e79c5
Sy Le <lenguyensy@gmail.com>
2022-06-04 11:45:40 -0700

    Now hide context menu for sidebar in modes where it isn't meaningful (#374)
    * callback to select stuffs
    
    * 1.32.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

---
+++ src/frontend/hooks/useTreeActions.tsx
@@ -0,0 +1,37 @@
+import { QueryClient, useQuery, useQueryClient } from 'react-query';
+import { useCallback } from 'react';
+
+const QUERY_KEY_TREE_ACTIONS = 'treeActions';
+
+type TreeActionProps = {
+  showContextMenu: boolean;
+  onSelectCallback?: (connectionId?: string, databaseId?: string, tableId?: string) => void;
+};
+
+// used for show and hide the sidebar trees
+let _treeActions: TreeActionProps = {
+  showContextMenu: true,
+  onSelectCallback: () => {},
+};
+
+export function useTreeActions() {
+  const queryClient = useQueryClient();
+
+  const { data, isLoading } = useQuery(QUERY_KEY_TREE_ACTIONS, () => _treeActions);
+
+  const setTreeActions = useCallback((newTreeActionProps: Partial<TreeActionProps>) => {
+    _treeActions.showContextMenu = newTreeActionProps.showContextMenu || false;
+    _treeActions.onSelectCallback = newTreeActionProps.onSelectCallback || undefined;
+    _treeActions = { ..._treeActions };
+    queryClient.setQueryData<TreeActionProps | undefined>(
+      QUERY_KEY_TREE_ACTIONS,
+      () => _treeActions,
+    );
+  }, []);
+
+  return {
+    isLoading,
+    data: data || _treeActions,
+    setTreeActions,
+  };
+}
