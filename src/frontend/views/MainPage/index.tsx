3cd9528dd1bfff7220ca34ddb6ee7cb8c98da11c
Sy Le <lenguyensy@gmail.com>
2022-06-18 09:27:28 -0700

    Use leetcode react resizer component to streamline our resizer and layout (#383)
    * use leet code resizer component
    
    * 1.34.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/views/MainPage/index.tsx
+++ src/frontend/views/MainPage/index.tsx
@@ -2,12 +2,10 @@ import { useEffect } from 'react';
 import ConnectionDescription from 'src/frontend/components/ConnectionDescription';
 import NewConnectionButton from 'src/frontend/components/NewConnectionButton';
 import QueryBoxTabs from 'src/frontend/components/QueryBoxTabs';
-import Resizer from 'src/frontend/components/Resizer';
-import { useSideBarWidthPreference } from 'src/frontend/hooks/useClientSidePreference';
 import { useTreeActions } from 'src/frontend/hooks/useTreeActions';
+import LayoutTwoColumns from 'src/frontend/layout/LayoutTwoColumns';
 
 export default function MainPage() {
-  const { value: width, onChange: onSetWidth } = useSideBarWidthPreference();
   const { setTreeActions } = useTreeActions();
 
   useEffect(() => {
@@ -17,15 +15,14 @@ export default function MainPage() {
   }, [setTreeActions]);
 
   return (
-    <section className='MainPage LayoutTwoColumns'>
-      <div className='LayoutTwoColumns__LeftPane' style={{ width }}>
+    <LayoutTwoColumns className='MainPage'>
+      <>
         <NewConnectionButton />
         <ConnectionDescription />
-      </div>
-      <Resizer onSetWidth={onSetWidth} />
-      <div className='LayoutTwoColumns__RightPane'>
+      </>
+      <>
         <QueryBoxTabs />
-      </div>
-    </section>
+      </>
+    </LayoutTwoColumns>
   );
 }
--- src/frontend/views/NewConnectionPage/index.tsx
+++ src/frontend/views/NewConnectionPage/index.tsx
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 import ConnectionDescription from 'src/frontend/components/ConnectionDescription';
 import { NewConnectionForm } from 'src/frontend/components/ConnectionForm';
 import NewConnectionButton from 'src/frontend/components/NewConnectionButton';
-import Resizer from 'src/frontend/components/Resizer';
 import { useSideBarWidthPreference } from 'src/frontend/hooks/useClientSidePreference';
 import { useTreeActions } from 'src/frontend/hooks/useTreeActions';
+import LayoutTwoColumns from 'src/frontend/layout/LayoutTwoColumns';
 
 export default function NewConnectionPage() {
   const { value: width, onChange: onSetWidth } = useSideBarWidthPreference();
@@ -19,18 +19,17 @@ export default function NewConnectionPage() {
   }, [setTreeActions]);
 
   return (
-    <section className='NewConnectionPage LayoutTwoColumns'>
-      <div className='LayoutTwoColumns__LeftPane' style={{ width }}>
+    <LayoutTwoColumns className='MainPage'>
+      <>
         <NewConnectionButton />
         <ConnectionDescription />
-      </div>
-      <Resizer onSetWidth={onSetWidth} />
-      <div className='LayoutTwoColumns__RightPane'>
+      </>
+      <>
         <Typography variant='h5' gutterBottom={true} sx={{ mt: 1 }}>
           New Connection
         </Typography>
         <NewConnectionForm />
-      </div>
-    </section>
+      </>
+    </LayoutTwoColumns>
   );
 }
