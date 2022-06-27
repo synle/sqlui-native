740062a7560c347c163441ed73c7c4f4d07f4d15
Sy Le <lenguyensy@gmail.com>
2022-06-26 20:28:32 -0700

    make backup for connection before deleting it

--- src/frontend/hooks/useConnection.tsx
+++ src/frontend/hooks/useConnection.tsx
@@ -2,6 +2,7 @@ import { QueryClient, useMutation, useQuery, useQueryClient } from 'react-query'
 import dataApi from 'src/frontend/data/api';
 import { getUpdatedOrdersForList } from 'src/frontend/utils/commonUtils';
 import { SqluiCore, SqluiFrontend } from 'typings';
+import {useAddRecycleBinItem} from 'src/frontend/hooks/useFolderItems';
 
 const QUERY_KEY_ALL_CONNECTIONS = 'connections';
 
@@ -84,6 +85,8 @@ export function useUpsertConnection() {
 
 export function useDeleteConnection() {
   const queryClient = useQueryClient();
+  const {mutateAsync: addRecycleBinItem} = useAddRecycleBinItem();
+  const {data: connections} = useGetConnections();
 
   return useMutation<string, void, string>(dataApi.deleteConnection, {
     onSuccess: async (deletedConnectionId) => {
@@ -96,6 +99,23 @@ export function useDeleteConnection() {
           return oldData?.filter((connection) => connection.id !== deletedConnectionId);
         },
       );
+
+      try{
+        // generate the connection backup to store in recyclebin
+        const connectionToBackup = connections?.find((connection) => connection.id === deletedConnectionId);
+
+        if(connectionToBackup){
+          // remove status before we backup.
+          const {status, ...restOfConnectionMetaData}  = connectionToBackup
+          await addRecycleBinItem({
+            type: 'Connection',
+            name: restOfConnectionMetaData.name,
+            data: restOfConnectionMetaData
+          })
+        }
+      } catch(err){
+        // TODO: add error handling
+      }
 
       return deletedConnectionId;
     },
