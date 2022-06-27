3fb1c25de28adc878fec937a6d1e6d7ef1baf32c
Sy Le <lenguyensy@gmail.com>
2022-06-26 19:38:17 -0700

    Folder API - prerequisite to handle bookmarks / recycleBin (#388)
    * Option to close tabs to the right of queries (#385)
    
    * close tabs to the right
    
    * add delete tabs to the right
    
    * minor clean up
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>
    
    * aip to handle recycle bin
    
    * 1.35.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/data/api.tsx
+++ src/frontend/data/api.tsx
@@ -159,12 +159,13 @@ export class ProxyApi {
       });
     }
   }
-
   static deleteSession(sessionId: string) {
     return _fetch<string>(`/api/session/${sessionId}`, {
       method: 'delete',
     }).then(() => sessionId);
   }
+
+  // used to read uploaded file content
   static readFileContent(file: File): Promise<string> {
     try {
       //@ts-ignore
@@ -178,6 +179,61 @@ export class ProxyApi {
         body: form,
       }).then((r) => r.text());
     }
+  }
+
+  // folders api
+  static getFolderItems(folderId: string) {
+    return _fetch<SqluiCore.Session[]>(`/api/folder/${folderId}`);
+  }
+
+  static addFolderItem(folderId: string, folderItem: SqluiCore.FolderItem) {
+    return _fetch<SqluiCore.ConnectionProps>(`/api/folder/${folderId}`, {
+      method: 'post',
+      body: JSON.stringify(folderItem),
+    });
+  }
+
+  static deleteFolderItem(folderId: SqluiCore.FolderType, itemId: string) {
+    return _fetch<string>(`/api/folder/${folderId}/${itemId}`, {
+      method: 'delete',
+    });
+  }
+
+  // recyclebin folder api
+  static getRecycleBinItems() {
+    return _fetch<SqluiCore.Session[]>(`/api/folder/recycleBin`);
+  }
+
+  static addRecycleBinItem(folderItem: SqluiCore.FolderItem) {
+    return _fetch<SqluiCore.ConnectionProps>(`/api/folder/recycleBin`, {
+      method: 'post',
+      body: JSON.stringify(folderItem),
+    });
+  }
+
+  static deleteRecycleBinItem(folderId: SqluiCore.FolderType, itemId: string) {
+    return _fetch<string>(`/api/folder/recycleBin/${itemId}`, {
+      method: 'delete',
+    });
+  }
+
+  // TODO: will be used to implement bookmarks
+  // bookmarks folder api
+  static getBookmarkItems() {
+    return _fetch<SqluiCore.Session[]>(`/api/folder/bookmarks`);
+  }
+
+  static addBookmarkItem(folderItem: SqluiCore.FolderItem) {
+    return _fetch<SqluiCore.ConnectionProps>(`/api/folder/bookmarks`, {
+      method: 'post',
+      body: JSON.stringify(folderItem),
+    });
+  }
+
+  static deleteBookmarkItem(folderId: SqluiCore.FolderType, itemId: string) {
+    return _fetch<string>(`/api/folder/bookmarks/${itemId}`, {
+      method: 'delete',
+    });
   }
 }
 
