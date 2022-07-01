5801d5b5dc69d74416d691622c644474432cd564
Sy Le <lenguyensy@gmail.com>
2022-07-01 14:14:57 -0700

    1.42.0

--- src/frontend/components/ColumnDescription/index.tsx
+++ src/frontend/components/ColumnDescription/index.tsx
@@ -1,5 +1,6 @@
 import ViewColumnIcon from '@mui/icons-material/ViewColumn';
 import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import React, { useEffect, useState } from 'react';
@@ -9,6 +10,7 @@ import ColumnName from 'src/frontend/components/ColumnDescription/ColumnName';
 import ColumnType from 'src/frontend/components/ColumnDescription/ColumnType';
 import { useGetColumns } from 'src/frontend/hooks/useConnection';
 import { useShowHide } from 'src/frontend/hooks/useShowHide';
+import { useActiveConnectionQuery } from 'src/frontend/hooks/useConnectionQuery';
 
 const MAX_COLUMN_SIZE_TO_SHOW = 5;
 
@@ -20,6 +22,7 @@ type ColumnDescriptionProps = {
 
 export default function ColumnDescription(props: ColumnDescriptionProps) {
   const { databaseId, connectionId, tableId } = props;
+  const { query: activeQuery } = useActiveConnectionQuery();
   const {
     data: columns,
     isLoading: loadingColumns,
@@ -64,15 +67,16 @@ export default function ColumnDescription(props: ColumnDescriptionProps) {
         .filter((column, idx) => showAllColumns || idx <= MAX_COLUMN_SIZE_TO_SHOW)
         .map((column) => {
           const key = [connectionId, databaseId, tableId, column.name].join(' > ');
+          const isSelected = visibles[key];
           return (
             <React.Fragment key={column.name}>
               <AccordionHeader
                 expanded={visibles[key]}
                 onToggle={() => onToggle(key)}
-                className='ColumnDescription'>
+                className={isSelected ? 'selected ColumnDescription' : 'ColumnDescription'}>
                 <ViewColumnIcon color='disabled' fontSize='inherit' />
-                <ColumnName value={column.name}></ColumnName>
-                <ColumnType value={column.type}></ColumnType>
+                  <ColumnName value={column.name}></ColumnName>
+                  <ColumnType value={column.type}></ColumnType>
               </AccordionHeader>
               <AccordionBody expanded={visibles[key]}>
                 <ColumnAttributes column={column} />
