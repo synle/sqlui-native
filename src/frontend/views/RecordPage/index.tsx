a1db4d4e7c50407fc6a6169ca95533890e6c8f3c
Sy Le <lenguyensy@gmail.com>
2022-07-07 12:27:20 -0700

    Aztable record page / new record / edit record (#443)
    * new record page and edit page for az table
    
    * record page
    
    * 1.44.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/views/RecordPage/index.tsx
+++ src/frontend/views/RecordPage/index.tsx
@@ -6,10 +6,16 @@ import TextField, { TextFieldProps } from '@mui/material/TextField';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import {
+  AZTABLE_KEYS_TO_IGNORE_FOR_INSERT_AND_UPDATE,
+  getInsert as getInsertForAzTable,
+  getUpdateWithValues as getUpdateWithValuesForAzTable,
+} from 'src/common/adapters/AzureTableStorageAdapter/scripts';
+import {
   getInsert as getInsertForRdmbs,
   getUpdateWithValues as getUpdateWithValuesForRmdbs,
 } from 'src/common/adapters/RelationalDataAdapter/scripts';
 import Breadcrumbs from 'src/frontend/components/Breadcrumbs';
+import CodeEditorBox from 'src/frontend/components/CodeEditorBox';
 import ConnectionDescription from 'src/frontend/components/ConnectionDescription';
 import JsonFormatData from 'src/frontend/components/JsonFormatData';
 import { useCommands } from 'src/frontend/components/MissionControl';
@@ -26,7 +32,7 @@ import {
 import useToaster from 'src/frontend/hooks/useToaster';
 import { useTreeActions } from 'src/frontend/hooks/useTreeActions';
 import LayoutTwoColumns from 'src/frontend/layout/LayoutTwoColumns';
-import { formatSQL } from 'src/frontend/utils/formatter';
+import { formatJS, formatSQL } from 'src/frontend/utils/formatter';
 import { SqluiCore, SqluiFrontend } from 'typings';
 
 type RecordData = any;
@@ -37,6 +43,7 @@ type RecordFormProps = {
   onConnectionChanges?: (query: Partial<SqluiFrontend.ConnectionQuery>) => void;
   query?: SqluiCore.ConnectionQuery;
   data?: RecordData;
+  rawValue?: string;
   isEditMode?: boolean;
 };
 
@@ -52,9 +59,9 @@ export function isRecordFormSupportedForDialect(dialect?: string) {
     case 'mongodb':
     case 'redis':
     case 'cosmosdb':
-    case 'aztable':
-    default:
       return false;
+    case 'aztable':
+      return true;
   }
 }
 
@@ -141,6 +148,7 @@ function RecordView(props: RecordDetailsPageProps) {
 function RecordForm(props) {
   const [searchParams, setSearchParams] = useSearchParams();
   const [deltaFields, setDeltaFields] = useState<Set<string>>(new Set());
+  const [rawValue, setRawValue] = useState('');
   const [data, setData] = useState<RecordData>(props.data || {});
   const [query, setQuery] = useState<Partial<SqluiFrontend.ConnectionQuery>>({
     id: 'migration_from_query_' + Date.now(),
@@ -200,20 +208,59 @@ function RecordForm(props) {
   // generate the dummy data when selector changes
   useEffect(() => {
     if (props.data) {
-      setData(props.data);
+      switch (connection?.dialect) {
+        case 'mysql':
+        case 'mariadb':
+        case 'mssql':
+        case 'postgres':
+        case 'sqlite':
+          setData(props.data);
+          break;
+        // case 'cassandra':
+        // case 'mongodb':
+        // case 'redis':
+        // case 'cosmosdb':
+        case 'aztable':
+          const newData = { ...props.data };
+          // for these, let's delete the system key
+          for (const keyToDelete of AZTABLE_KEYS_TO_IGNORE_FOR_INSERT_AND_UPDATE) {
+            delete newData[keyToDelete];
+          }
+          setRawValue(JSON.stringify(newData, null, 2));
+          break;
+      }
       return;
     }
 
     const newData = {};
-
     if (columns) {
-      for (const column of columns) {
-        newData[column.name] = '';
+      switch (connection?.dialect) {
+        case 'mysql':
+        case 'mariadb':
+        case 'mssql':
+        case 'postgres':
+        case 'sqlite':
+          for (const column of columns) {
+            newData[column.name] = '';
+          }
+          setData(newData);
+          break;
+        // case 'cassandra':
+        // case 'mongodb':
+        // case 'redis':
+        // case 'cosmosdb':
+        case 'aztable':
+          for (const column of columns.filter(
+            (targetColumn) =>
+              AZTABLE_KEYS_TO_IGNORE_FOR_INSERT_AND_UPDATE.indexOf(targetColumn.name) === -1,
+          )) {
+            newData[column.name] = '';
+          }
+          setRawValue(JSON.stringify(newData, null, 2));
+          break;
       }
     }
-
-    setData(newData);
-  }, [columns, props.data]);
+  }, [connection, columns, props.data]);
 
   useEffect(() => {
     if (props.isEditMode === true) {
@@ -234,6 +281,16 @@ function RecordForm(props) {
       <React.Fragment key='non_supported_dialect'>
         The dialect of this connection is not supported for RecordForm
       </React.Fragment>,
+    );
+  } else if (connection?.dialect === 'aztable') {
+    // js raw value
+    contentFormDataView.push(
+      <CodeEditorBox
+        key='rawValue'
+        value={rawValue}
+        onChange={(newValue) => setRawValue(newValue)}
+        language='js'
+      />,
     );
   } else if (columns && columns.length > 0) {
     for (const column of columns.sort((a, b) => a.name.localeCompare(b.name))) {
@@ -282,6 +339,7 @@ function RecordForm(props) {
           connection,
           columns,
           data,
+          rawValue,
           deltaFields: [...deltaFields],
         });
       }}>
@@ -319,7 +377,7 @@ export function NewRecordPage() {
   const { add: addToast } = useToaster();
   const { selectCommand } = useCommands();
 
-  const onSave = async ({ query, connection, columns, data }) => {
+  const onSave = async ({ query, connection, columns, data, rawValue }) => {
     let sql: string = '';
     switch (connection?.dialect) {
       case 'mysql':
@@ -342,11 +400,31 @@ export function NewRecordPage() {
       // case 'mongodb':
       // case 'redis':
       // case 'cosmosdb':
-      // case 'aztable':
+      case 'aztable':
+        try {
+          const jsonValue = JSON.parse(rawValue);
+
+          sql = formatJS(
+            getInsertForAzTable(
+              {
+                ...query,
+                dialect: connection.dialect,
+                columns,
+              },
+              jsonValue,
+            )?.query || '',
+          );
+        } catch (err) {
+          await addToast({
+            message:
+              'Azure Table value needs to be a valid JSON object. Input provided is not a valid JSON...',
+          });
+          return;
+        }
+        break;
       // default:
       //   break;
     }
-
     onAddQuery({
       name: `New Record Query - ${new Date().toLocaleDateString()}`,
       ...query,
@@ -410,42 +488,40 @@ export function EditRecordPage(props: RecordDetailsPageProps) {
   const { add: addToast } = useToaster();
   // TODO: intelligently pick up the table name from schema of data
 
-  const onSave = async ({ query, connection, columns, data, deltaFields }) => {
-    // TODO: to be implemented
-    setIsEdit(false);
-
+  const onSave = async ({ query, connection, columns, data, rawValue, deltaFields }) => {
     let sql = '';
+
+    // find out the delta
+    let deltaData = {};
+    for (const deltaField of deltaFields) {
+      deltaData[deltaField] = data[deltaField];
+    }
+    if (Object.keys(deltaData).length === 0) {
+      deltaData = data;
+    }
+    // find out the main condition
+    const conditions = {};
+    for (const column of columns) {
+      if (column.primaryKey) {
+        conditions[column.name] = data[column.name];
+      }
+    }
+
+    if (Object.keys(conditions).length === 0) {
+      for (const column of columns) {
+        if (column.name?.toString().toLowerCase()?.includes('id')) {
+          // otherwise include any of the id
+          conditions[column.name] = data[column.name];
+        }
+      }
+    }
+
     switch (connection?.dialect) {
       case 'mysql':
       case 'mariadb':
       case 'mssql':
       case 'postgres':
       case 'sqlite':
-        // find out the delta
-        let deltaData = {};
-        for (const deltaField of deltaFields) {
-          deltaData[deltaField] = data[deltaField];
-        }
-        if (Object.keys(deltaData).length === 0) {
-          deltaData = data;
-        }
-        // find out the main condition
-        const conditions = {};
-        for (const column of columns) {
-          if (column.primaryKey) {
-            conditions[column.name] = data[column.name];
-          }
-        }
-
-        if (Object.keys(conditions).length === 0) {
-          for (const column of columns) {
-            if (column.name?.toString().toLowerCase()?.includes('id')) {
-              // otherwise include any of the id
-              conditions[column.name] = data[column.name];
-            }
-          }
-        }
-
         sql = formatSQL(
           getUpdateWithValuesForRmdbs(
             {
@@ -457,12 +533,37 @@ export function EditRecordPage(props: RecordDetailsPageProps) {
             conditions,
           )?.query || '',
         );
+        setIsEdit(false);
         break;
       // case 'cassandra':
       // case 'mongodb':
       // case 'redis':
       // case 'cosmosdb':
-      // case 'aztable':
+      case 'aztable':
+        try {
+          const jsonValue = JSON.parse(rawValue);
+
+          sql = formatJS(
+            getUpdateWithValuesForAzTable(
+              {
+                ...query,
+                dialect: connection.dialect,
+                columns,
+              },
+              jsonValue,
+              conditions,
+            )?.query || '',
+          );
+
+          setIsEdit(false);
+        } catch (err) {
+          await addToast({
+            message:
+              'Azure Table value needs to be a valid JSON object. Input provided is not a valid JSON...',
+          });
+          return;
+        }
+        break;
       // default:
       //   break;
     }
