f4c78cc0d20d3c7dbd7718281720fa000bd1af8f
Sy Le <lenguyensy@gmail.com>
2022-07-08 09:08:14 -0700

    added create and drop table

--- src/common/adapters/CassandraDataAdapter/scripts.ts
+++ src/common/adapters/CassandraDataAdapter/scripts.ts
@@ -10,15 +10,13 @@ export function getSampleConnectionString(dialect?: SqluiCore.Dialect) {
 export function getSelectAllColumns(input: SqlAction.TableInput): SqlAction.Output | undefined {
   const label = `Select All Columns`;
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `SELECT *
+  return {
+    label,
+    formatter,
+    query: `SELECT *
               FROM ${input.tableId}
               LIMIT ${input.querySize}`,
-    };
-  }
+  };
 }
 
 export function getSelectSpecificColumns(
@@ -33,16 +31,14 @@ export function getSelectSpecificColumns(
   const columnString = `\n` + input.columns.map((col) => `  ${col.name}`).join(',\n');
   const whereColumnString = input.columns.map((col) => `${col.name} = ''`).join('\n AND ');
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `SELECT ${columnString}
+  return {
+    label,
+    formatter,
+    query: `SELECT ${columnString}
               FROM ${input.tableId}
               WHERE ${whereColumnString}
               LIMIT ${input.querySize}`,
-    };
-  }
+  };
 }
 
 export function getInsert(input: SqlAction.TableInput): SqlAction.Output | undefined {
@@ -55,14 +51,12 @@ export function getInsert(input: SqlAction.TableInput): SqlAction.Output | undef
   const columnString = input.columns.map((col) => col.name).join(',\n');
   const insertValueString = input.columns.map((col) => `'_${col.name}_'`).join(',\n');
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `INSERT INTO ${input.tableId} (${columnString})
+  return {
+    label,
+    formatter,
+    query: `INSERT INTO ${input.tableId} (${columnString})
               VALUES (${insertValueString})`,
-    };
-  }
+  };
 }
 
 export function getUpdate(input: SqlAction.TableInput): SqlAction.Output | undefined {
@@ -75,15 +69,13 @@ export function getUpdate(input: SqlAction.TableInput): SqlAction.Output | undef
   const columnString = input.columns.map((col) => `${col.name} = ''`).join(',\n');
   const whereColumnString = input.columns.map((col) => `${col.name} = ''`).join(' AND \n');
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter: 'js',
-      query: `UPDATE ${input.tableId}
+  return {
+    label,
+    formatter: 'js',
+    query: `UPDATE ${input.tableId}
               SET ${columnString}
               WHERE ${whereColumnString}`,
-    };
-  }
+  };
 }
 
 export function getDelete(input: SqlAction.TableInput): SqlAction.Output | undefined {
@@ -95,39 +87,33 @@ export function getDelete(input: SqlAction.TableInput): SqlAction.Output | undef
 
   const whereColumnString = input.columns.map((col) => `${col.name} = ''`).join(' AND \n');
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `DELETE FROM ${input.tableId}
+  return {
+    label,
+    formatter,
+    query: `DELETE FROM ${input.tableId}
               WHERE ${whereColumnString}`,
-    };
-  }
+  };
 }
 
 export function getCreateKeyspace(input: SqlAction.DatabaseInput): SqlAction.Output | undefined {
   const label = `Create Keyspace`;
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `CREATE KEYSPACE IF NOT EXISTS some_keyspace
+  return {
+    label,
+    formatter,
+    query: `CREATE KEYSPACE IF NOT EXISTS some_keyspace
              WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};`,
-    };
-  }
+  };
 }
 
 export function getDropKeyspace(input: SqlAction.DatabaseInput): SqlAction.Output | undefined {
   const label = `Drop Keyspace`;
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `DROP KEYSPACE IF EXISTS ${input.databaseId};`,
-    };
-  }
+  return {
+    label,
+    formatter,
+    query: `DROP KEYSPACE IF EXISTS ${input.databaseId};`,
+  };
 }
 
 export function getCreateConnectionDatabase(
@@ -135,14 +121,12 @@ export function getCreateConnectionDatabase(
 ): SqlAction.Output | undefined {
   const label = `Create Connection Keyspace`;
 
-  if (input.dialect === 'cassandra') {
-    return {
-      label,
-      formatter,
-      query: `CREATE KEYSPACE IF NOT EXISTS some_keyspace
-             WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};`,
-    };
-  }
+  return {
+    label,
+    formatter,
+    query: `CREATE KEYSPACE IF NOT EXISTS some_keyspace
+           WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};`,
+  };
 }
 
 export const tableActionScripts: SqlAction.TableActionScriptGenerator[] = [
