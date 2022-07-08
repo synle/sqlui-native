90e2ebfc222e1e1dd0a3c95be3b92443caedcf8f
Sy Le <lenguyensy@gmail.com>
2022-07-08 11:15:16 -0700

    Consolidate sorting of the names of databases, tables and columns inside of the base data adapter. (#453)
    * consolidate code for get column and get table
    
    * added sortings
    
    * more unifying sorting
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/common/adapters/DataAdapterFactory.ts
+++ src/common/adapters/DataAdapterFactory.ts
@@ -6,6 +6,7 @@ import IDataAdapter from 'src/common/adapters/IDataAdapter';
 import MongoDBDataAdapter from 'src/common/adapters/MongoDBDataAdapter/index';
 import RedisDataAdapter from 'src/common/adapters/RedisDataAdapter/index';
 import RelationalDataAdapter from 'src/common/adapters/RelationalDataAdapter/index';
+import PersistentStorage from 'src/common/PersistentStorage';
 import { SqluiCore } from 'typings';
 
 const _adapterCache: { [index: string]: IDataAdapter } = {};
@@ -111,4 +112,62 @@ export function resetConnectionMetaData(connection: SqluiCore.CoreConnectionProp
   delete _adapterCache[connection.connection];
 
   return connItem;
+}
+
+export async function getDatabases(sessionId: string, connectionId: string) {
+  const connection = await new PersistentStorage<SqluiCore.ConnectionProps>(
+    sessionId,
+    'connection',
+  ).get(connectionId);
+
+  return (await getDataAdapter(connection.connection).getDatabases()).sort((a, b) =>
+    (a.name || '').localeCompare(b.name || ''),
+  );
+}
+
+export async function getTables(sessionId: string, connectionId: string, databaseId: string) {
+  const connection = await new PersistentStorage<SqluiCore.ConnectionProps>(
+    sessionId,
+    'connection',
+  ).get(connectionId);
+
+  return (await getDataAdapter(connection.connection).getTables(databaseId)).sort((a, b) =>
+    (a.name || '').localeCompare(b.name || ''),
+  );
+}
+
+export async function getColumns(
+  sessionId: string,
+  connectionId: string,
+  databaseId: string,
+  tableId: string,
+) {
+  const connection = await new PersistentStorage<SqluiCore.ConnectionProps>(
+    sessionId,
+    'connection',
+  ).get(connectionId);
+
+  return (await getDataAdapter(connection.connection).getColumns(tableId, databaseId)).sort(
+    (a, b) => {
+      const aPrimaryKey = a.primaryKey || a.kind === 'partition_key';
+      const bPrimaryKey = b.primaryKey || b.kind === 'partition_key';
+
+      if (aPrimaryKey !== bPrimaryKey) {
+        return aPrimaryKey ? -1 : 1;
+      }
+
+      if (a.unique !== b.unique) {
+        return a.unique ? -1 : 1;
+      }
+
+      const aClusterKey = a.kind === 'clustering';
+      const bClusterKey = b.kind === 'clustering';
+
+      if (aClusterKey !== bClusterKey) {
+        return aClusterKey ? -1 : 1;
+      }
+
+      return (a.name || '').localeCompare(b.name || '');
+    },
+  );
 }
