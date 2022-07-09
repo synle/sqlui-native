7d3034e87ce0dadb16941ad8942608b56ab6fee3
Sy Le <lenguyensy@gmail.com>
2022-07-09 08:19:47 -0700

    minor clean up

--- src/frontend/utils/commonUtils.spec.tsx
+++ src/frontend/utils/commonUtils.spec.tsx
@@ -76,54 +76,59 @@ FROM
   });
 
   describe('getUpdatedOrdersForList', () => {
-    const items = [11, 22, 33, 44, 55];
+    let items;
+
+    beforeEach(() => {
+      items = [11, 22, 33, 44, 55]
+    })
+
     test('should work for from=4, to=2', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 4, 2);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 4, 2);
       expect(actual.join(',')).toMatchInlineSnapshot(`"11,22,55,33,44"`);
     });
 
     test('should work for from=4, to=3', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 4, 3);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 4, 3);
       expect(actual.join(',')).toMatchInlineSnapshot(`"11,22,33,55,44"`);
     });
 
     test('should work for from=4, to=0', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 4, 0);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 4, 0);
       expect(actual.join(',')).toMatchInlineSnapshot(`"55,11,22,33,44"`);
     });
 
     test('should work for from=0, to=1', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 0, 1);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 0, 1);
       expect(actual.join(',')).toMatchInlineSnapshot(`"22,11,33,44,55"`);
     });
 
     test('should work for from=0, to=4', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 0, 4);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 0, 4);
       expect(actual.join(',')).toMatchInlineSnapshot(`"22,33,44,55,11"`);
     });
 
     test('should work for from=0, to=3', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 0, 3);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 0, 3);
       expect(actual.join(',')).toMatchInlineSnapshot(`"22,33,44,11,55"`);
     });
 
     test('should work for from=1, to=3', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 1, 3);
+      let actual = commonUtils.getUpdatedOrdersForList(items, 1, 3);
       expect(actual.join(',')).toMatchInlineSnapshot(`"11,33,44,22,55"`);
     });
 
     test('should work for from=0, to=0 (no change in order)', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 0, 0)
+      let actual = commonUtils.getUpdatedOrdersForList(items, 0, 0)
       expect(actual.join(',')).toMatchInlineSnapshot(`"11,22,33,44,55"`);
     });
 
     test('should work for from=3, to=3 (no change in order)', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 3, 3)
+      let actual = commonUtils.getUpdatedOrdersForList(items, 3, 3)
       expect(actual.join(',')).toMatchInlineSnapshot(`"11,22,33,44,55"`);
     });
 
     test('should work for from=4, to=4 (no change in order)', async () => {
-      let actual = commonUtils.getUpdatedOrdersForList([...items], 4, 4)
+      let actual = commonUtils.getUpdatedOrdersForList(items, 4, 4)
       expect(actual.join(',')).toMatchInlineSnapshot(`"11,22,33,44,55"`);
     });
   });
