a09dac46a29b559573aad86262ea97e84a6baee4
Sy Le <lenguyensy@gmail.com>
2022-07-11 12:58:34 -0700

    Fixed minor issue where js scripts are not formatting after recent refactoring (#481)
    * fixed minor issue with format js in script
    
    * Fixed minor issue where js scripts are not formatting after recent refactoring
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/utils/formatter.spec.tsx
+++ src/frontend/utils/formatter.spec.tsx
@@ -6,9 +6,18 @@ describe('formatter', () => {
       const actual = formatter.formatJS(
         `db.collection("sy-collection-1a").distinct("commute",{_id:"",commute:"","company.location":"","company.name":"",location:"",name:"",zip:""});`,
       );
-      expect(actual).toMatchInlineSnapshot(
-        `"db.collection(\\"sy-collection-1a\\").distinct(\\"commute\\",{_id:\\"\\",commute:\\"\\",\\"company.location\\":\\"\\",\\"company.name\\":\\"\\",location:\\"\\",name:\\"\\",zip:\\"\\"});"`,
-      );
+      expect(actual).toMatchInlineSnapshot(`
+"db.collection(\\"sy-collection-1a\\")
+  .distinct(\\"commute\\", {
+    _id: \\"\\",
+    commute: \\"\\",
+    \\"company.location\\": \\"\\",
+    \\"company.name\\": \\"\\",
+    location: \\"\\",
+    name: \\"\\",
+    zip: \\"\\"
+  });"
+`);
     });
   });
 
