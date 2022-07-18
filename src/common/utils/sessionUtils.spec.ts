ef7643c14b264ff43969f1771833a3a47eb7fb2f
Sy Le <lenguyensy@gmail.com>
2022-07-17 19:17:24 -0700

    added unit tests

--- src/common/utils/sessionUtils.spec.ts
+++ src/common/utils/sessionUtils.spec.ts
@@ -7,6 +7,7 @@ describe('sessionUtils', () => {
   const s1 = 'session_1';
   const s2 = 'session_2';
   const s3 = 'session_3';
+  const s4 = 'session_4';
 
   beforeEach(() => {
     sessionUtils.reset();
@@ -109,4 +110,17 @@ Object {
     sessionUtils.close(w1);
     expect(sessionUtils.get()).toEqual({});
   });
+
+  test('getWindowIdBySessionId', async () => {
+    expect(sessionUtils.get()).toEqual({});
+
+    sessionUtils.open(w1, s1);
+    sessionUtils.open(w2, s2);
+    sessionUtils.open(w3, s3);
+
+    expect(sessionUtils.getWindowIdBySessionId(s1)).toMatchInlineSnapshot(`"window_1"`);
+    expect(sessionUtils.getWindowIdBySessionId(s2)).toMatchInlineSnapshot(`"window_2"`);
+    expect(sessionUtils.getWindowIdBySessionId(s3)).toMatchInlineSnapshot(`"window_3"`);
+    expect(sessionUtils.getWindowIdBySessionId(s4)).toMatchInlineSnapshot(`undefined`);
+  })
 });
