889f1c8533f79d7709709a3e2600dfd0bf3ff23e
Sy Le <lenguyensy@gmail.com>
2022-07-18 20:20:24 -0700

    Now run prettier format scripts on all tests (#508)
    * now format test as well
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/common/adapters/BaseDataAdapter/index.inferSqlTypeFromItems.spec.ts
+++ src/common/adapters/BaseDataAdapter/index.inferSqlTypeFromItems.spec.ts
@@ -2,42 +2,42 @@ import BaseDataAdapter from 'src/common/adapters/BaseDataAdapter/index';
 
 describe('BaseDataAdapter', () => {
   describe('inferSqlTypeFromItems', () => {
-      const mockedObjects = [
-        {
-          "cb": 22.22,
-          "cd": "asdsadsfdsafsadfdsa"
+    const mockedObjects = [
+      {
+        cb: 22.22,
+        cd: 'asdsadsfdsafsadfdsa',
+      },
+      {
+        ca: 115,
+        cc: true,
+        cd: 'asdsadsfdsafsadfdsa',
+      },
+      {
+        n: {
+          na: 'aa',
+          nb: 22,
+          nc: {
+            nc1: 99,
+            nc2: true,
+          },
         },
-        {
-          "ca": 115,
-          "cc": true,
-          "cd": "asdsadsfdsafsadfdsa"
-        },
-        {
-          n: {
-            na: 'aa',
-            nb: 22,
-            nc: {
-              nc1: 99,
-              nc2: true
-            }
-          }
-        }
-      ];
+      },
+    ];
 
-      test.each([
-        ['mysql'],
-        ['mariadb'],
-        ['mssql'],
-        ['postgres'],
-        ['sqlite'],
-        ['cassandra'],
-        ['mongodb'],
-        ['redis'],
-        ['cosmosdb'],
-        ['aztable'],
-      ])('inferSqlTypeFromItems work correctly for dialect=%s',(inputDialect) =>{
-          const actual = BaseDataAdapter.inferSqlTypeFromItems(mockedObjects, inputDialect);
-        expect(actual).toMatchSnapshot();
-      })
+    test.each([
+      ['mysql'],
+      ['mariadb'],
+      ['mssql'],
+      ['postgres'],
+      ['sqlite'],
+      ['cassandra'],
+      ['mongodb'],
+      ['redis'],
+      ['cosmosdb'],
+      ['aztable'],
+    ])('inferSqlTypeFromItems work correctly for dialect=%s', (inputDialect) => {
+      const actual = BaseDataAdapter.inferSqlTypeFromItems(mockedObjects, inputDialect);
+      expect(actual).toMatchSnapshot();
+    });
   });
 });
