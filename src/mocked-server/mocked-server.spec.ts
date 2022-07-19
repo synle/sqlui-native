0d76b03365a88b39ff7237a4f0823ea5d5664489
Sy Le <lenguyensy@gmail.com>
2022-07-18 20:05:50 -0700

    Added first integration test for the API using supertest (#507)
    * refactor to test backend endpoints
    
    * wip adding unit test for the endpoints
    
    * wip
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

---
+++ src/mocked-server/mocked-server.spec.ts
@@ -0,0 +1,14 @@
+import supertest from 'supertest';
+import { app } from 'src/mocked-server/mocked-server';
+
+const requestWithSupertest = supertest(app);
+
+describe('Configs', () => {
+  it('GET /api/configs should work', async () => {
+    const res = await requestWithSupertest.get('/api/configs');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toHaveProperty('storageDir');
+    expect(res.body).toHaveProperty('isElectron')
+  });
+});
