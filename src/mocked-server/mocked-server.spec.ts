6171e43da76660bf2d6b98a97f3e57fcc99dff70
Sy Le <lenguyensy@gmail.com>
2022-07-18 20:44:10 -0700

    Added more integration tests (#509)
    * more tests
    
    * session tests
    
    * more tests
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/mocked-server/mocked-server.spec.ts
+++ src/mocked-server/mocked-server.spec.ts
@@ -12,3 +12,60 @@ describe('Configs', () => {
     expect(res.body).toHaveProperty('isElectron');
   });
 });
+
+describe('Sessions', () => {
+  const mockedSessionId = `mocked-session-id.${Date.now()}`;
+  const mockedSessionValue1 = {
+    id: mockedSessionId,
+    name: 'Mocked Session Name Value 1',
+  };
+
+  const mockedSessionValue2 = {
+    id: mockedSessionId,
+    name: 'Mocked Session Name Value 2',
+  };
+
+  const mockedConnection1 = {
+    name: 'mysql Connection - 7/18/2022',
+    connection: 'mysql://root:password@localhost:3306',
+  };
+
+  const commonHeaders = {
+    'sqlui-native-session-id': mockedSessionId,
+    'sqlui-native-window-id': 'mocked-window-id',
+  };
+  it('Simple scenario Create Session / Get Session', async () => {
+    // here we create the session
+    let res: any;
+    res = await requestWithSupertest
+      .put(`/api/session/${mockedSessionId}`)
+      .send(mockedSessionValue1);
+    expect(res.status).toEqual(202);
+
+    res = await requestWithSupertest.get('/api/sessions');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toContainEqual(mockedSessionValue1);
+    expect(res.body.length > 0).toEqual(true);
+    // rename the session
+    res = await requestWithSupertest
+      .put(`/api/session/${mockedSessionId}`)
+      .send(mockedSessionValue2);
+    expect(res.status).toEqual(202);
+
+    res = await requestWithSupertest.get('/api/sessions');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toContainEqual(mockedSessionValue2);
+  });
+
+  it('Simple Connection & Query', async () => {
+    let res: any;
+    res = await requestWithSupertest
+      .post('/api/connection')
+      .set(commonHeaders)
+      .send(mockedConnection1);
+    expect(res.status).toEqual(201);
+    expect(res.body).toEqual(expect.objectContaining(mockedConnection1));
+  });
+});
