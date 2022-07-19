457c21e33c16b1aa8243f7afb79f210763c64dc4
Sy Le <lenguyensy@gmail.com>
2022-07-18 20:21:12 -0700

    more tests

--- src/mocked-server/mocked-server.spec.ts
+++ src/mocked-server/mocked-server.spec.ts
@@ -12,3 +12,22 @@ describe('Configs', () => {
     expect(res.body).toHaveProperty('isElectron');
   });
 });
+
+describe('Sessions', () => {
+  const mockedSessionId = `mocked-session-id.${Date.now()}`;
+  const mockedSessionValue = {
+    "id":mockedSessionId,
+    "name":"Mocked Session Name"
+  }
+
+  it('Simple scenario', async () => {
+    let res: any;
+    res = await requestWithSupertest.put(`/api/session/${mockedSessionId}`).send(mockedSessionValue);
+    expect(res.status).toEqual(202);
+
+    res = await requestWithSupertest.get('/api/sessions');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toContainEqual(mockedSessionValue);
+  });
+});
