8889b4a9145682e6620751d056d640340a5924fb
Sy Le <lenguyensy@gmail.com>
2022-07-22 08:50:39 -0700

    fixed cloned

--- src/frontend/components/MissionControl/index.tsx
+++ src/frontend/components/MissionControl/index.tsx
@@ -466,7 +466,7 @@ export default function MissionControl() {
       }
 
       const newSession = await cloneSession({
-        id: getRandomSessionId(),
+        id: targetSession.id,
         name: newSessionName,
       });
 
