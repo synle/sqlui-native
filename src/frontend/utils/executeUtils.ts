731301502b9dd4c5751108e9c8a9caf715a1e0ee
Sy Le <lenguyensy@gmail.com>
2022-07-28 08:11:02 -0700

    Fixed dev mode not running
    Uncaught TypeError: window.requireElectron is not a function

--- src/frontend/utils/executeUtils.ts
+++ src/frontend/utils/executeUtils.ts
@@ -1,7 +1,11 @@
-// @ts-ignore
-const { exec } = window.requireElectron('child_process');
-
 export function execute(shellToRun: string, delay = 25): Promise<string> {
+  if(!window.isElectron){
+    return Promise.resolve('');
+  }
+
+  // @ts-ignore
+  const { exec } = window.requireElectron('child_process');
+
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       exec(shellToRun, (error, stdout, stderr) => {
