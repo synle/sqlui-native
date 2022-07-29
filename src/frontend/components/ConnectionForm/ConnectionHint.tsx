fcf6ce4364e9d76cb275f37fa67472f652735ec5
Sy Le <lenguyensy@gmail.com>
2022-07-29 06:47:32 -0700

    Show bookmarked connections in the new connection form (#539)
    * Fixed issue with parser for encoded string
    
    Allow     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
    
    fixed the parser encoded
    
    * CI / CD - Format Updates Automatic Commit  [skip ci]
    
    * added default port
    
    * update readme
    
    * 1.56.0
    
    * clean up and introduce bookmarks as part of the new connection page
    
    * CI / CD - Format Updates Automatic Commit  [skip ci]
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/components/ConnectionForm/ConnectionHint.tsx
+++ src/frontend/components/ConnectionForm/ConnectionHint.tsx
@@ -1,5 +1,7 @@
-import { Link } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
+import { Box, Link, Skeleton } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
@@ -9,17 +11,80 @@ import {
   SUPPORTED_DIALECTS,
 } from 'src/common/adapters/DataScriptFactory';
 import ConnectionTypeIcon from 'src/frontend/components/ConnectionTypeIcon';
+import { useGetBookmarkItems } from 'src/frontend/hooks/useFolderItems';
+import { SqluiCore } from 'typings';
 
 type ConnectionHintProps = {
+  showBookmarks?: boolean;
   onChange: (dialect: string, connection: string) => void;
 };
 
 export default function ConnectionHint(props: ConnectionHintProps) {
+  const { data, isLoading } = useGetBookmarkItems();
+  let bookmarkedConnectionsDom: any[] = [];
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Skeleton variant='rectangular' height={40} />
+        <Skeleton variant='rectangular' height={40} />
+        <Skeleton variant='rectangular' height={40} />
+        <Skeleton variant='rectangular' height={40} />
+        <Skeleton variant='rectangular' height={40} />
+      </Box>
+    );
+  }
+
+  const connectionsFromBookmark = data?.filter((bookmark) => bookmark.type === 'Connection');
+
+  if (
+    props.showBookmarks === true &&
+    connectionsFromBookmark &&
+    connectionsFromBookmark.length > 0
+  ) {
+    // pulling the connection string from bookmarks
+    bookmarkedConnectionsDom = connectionsFromBookmark.map((connection) => {
+      const bookmark = connection.data as SqluiCore.ConnectionProps;
+      const dialect = bookmark.dialect || '';
+      const connectionToUse = bookmark.connection;
+
+      const onApplyThisConnectionHint = () => props.onChange(dialect, connectionToUse);
+
+      return (
+        <ListItem key={bookmark.id}>
+          <ListItemAvatar>
+            <Avatar>
+              <ConnectionTypeIcon dialect={dialect} status={dialect ? 'online' : ''} />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={
+              <Box sx={{ display: 'flex', gap: 0.5, alignItems: 'center' }}>
+                <StarIcon fontSize='small' />
+                <Link
+                  underline='hover'
+                  onClick={onApplyThisConnectionHint}
+                  sx={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
+                  {connection.name}
+                </Link>
+              </Box>
+            }
+            secondary={connectionToUse}
+          />
+        </ListItem>
+      );
+    });
+
+    // add the divider in front
+    bookmarkedConnectionsDom.unshift(<Divider key='divider' />);
+  }
+
   return (
-    <List sx={{ bgcolor: 'background.paper' }}>
+    <List sx={{ bgcolor: 'background.paper', overflow: 'hidden', wordBreak: 'break-all' }}>
       {SUPPORTED_DIALECTS.map((dialect) => {
         const onApplyThisConnectionHint = () =>
           props.onChange(dialect, getSampleConnectionString(dialect));
+
         return (
           <ListItem key={dialect}>
             <ListItemAvatar>
@@ -41,6 +106,7 @@ export default function ConnectionHint(props: ConnectionHintProps) {
           </ListItem>
         );
       })}
+      {bookmarkedConnectionsDom}
     </List>
   );
 }
