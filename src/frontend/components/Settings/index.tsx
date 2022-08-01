1bd302f2e8ebcc3e54a83737eb066dfaeb5f6658
Sy Le <lenguyensy@gmail.com>
2022-07-31 17:03:16 -0700

    Now use `virtualize` to render large data lists faster (#542)
    * virtualize table
    
    * wip
    
    * wip working
    
    * wip
    
    * more presentatble virtualized
    
    * wip
    
    * wip styling
    
    * wip
    
    * now properly set up the timer
    
    * return total count
    
    * wip
    
    * 1.57.0
    
    * CI / CD - Format Updates Automatic Commit
    
    * show total records
    
    * added the accessor
    
    * added search global
    
    * properly use the accessor value
    
    * added showGlobalSearch
    
    * remove page size on settings page
    
    * wip
    
    * Revert "wip"
    
    This reverts commit 528fece03fb1cf5acb721f7a9a8deb04a7b23ee5.
    
    * CI / CD - Format Updates Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/components/Settings/index.tsx
+++ src/frontend/components/Settings/index.tsx
@@ -127,24 +127,26 @@ export default function Settings(props: SettingsProps): JSX.Element | null {
             type='number'
           />
         </div>
-        <Typography className='FormInput__Label' variant='subtitle1'>
-          Table Page Size
-          <Tooltip title='The default table page size used for results table. Note this change only apply to future results.'>
-            <HelpIcon fontSize='small' sx={{ ml: 1 }} />
-          </Tooltip>
-        </Typography>
-        <div className='FormInput__Row'>
-          <Select
-            value={settings.tablePageSize || DEFAULT_TABLE_PAGE_SIZE}
-            onChange={(newValue) => onSettingChange('tablePageSize', newValue)}
-            sx={{ width: '100%' }}>
-            {ALL_PAGE_SIZE_OPTIONS.map((pageSize) => (
-              <option value={pageSize.value} key={pageSize.value}>
-                {pageSize.label}
-              </option>
-            ))}
-          </Select>
-        </div>
+        {/*
+          <Typography className='FormInput__Label' variant='subtitle1'>
+            Table Page Size
+            <Tooltip title='The default table page size used for results table. Note this change only apply to future results.'>
+              <HelpIcon fontSize='small' sx={{ ml: 1 }} />
+            </Tooltip>
+          </Typography>
+          <div className='FormInput__Row'>
+            <Select
+              value={settings.tablePageSize || DEFAULT_TABLE_PAGE_SIZE}
+              onChange={(newValue) => onSettingChange('tablePageSize', newValue)}
+              sx={{ width: '100%' }}>
+              {ALL_PAGE_SIZE_OPTIONS.map((pageSize) => (
+                <option value={pageSize.value} key={pageSize.value}>
+                  {pageSize.label}
+                </option>
+              ))}
+            </Select>
+          </div>
+        */}
         <Typography className='FormInput__Label' variant='subtitle1'>
           Delete Mode
           <Tooltip title='Whether or not to do soft delete and put deleted items into the recycle bin or hard delete to delete it permanently.'>
