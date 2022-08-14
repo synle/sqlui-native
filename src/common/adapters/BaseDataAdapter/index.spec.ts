93225771c40adab648f6c5cbfe636cb942c4442d
Sy Le <lenguyensy@gmail.com>
2022-08-14 08:04:31 -0700

    Added more unit tests for schema parser (#605)
    * added more unit tests
    
    * more tests
    
    * CI / CD - Format Updates Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/common/adapters/BaseDataAdapter/index.spec.ts
+++ src/common/adapters/BaseDataAdapter/index.spec.ts
@@ -4,7 +4,7 @@ describe('BaseDataAdapter', () => {
   describe('getConnectionParameters', () => {
     test('basic input should work', async () => {
       const config = BaseDataAdapter.getConnectionParameters('cassandra://localhost:9042');
-      expect(config?.scheme).toBe('cassandra');
+      expect(config?.scheme).toMatchInlineSnapshot(`"cassandra"`);
       expect(config?.hosts).toMatchInlineSnapshot(`
 Array [
   Object {
@@ -19,8 +19,9 @@ Array [
       const config = BaseDataAdapter.getConnectionParameters(
         'cassandra://localhost:9042/system_schema',
       );
-      expect(config?.scheme).toBe('cassandra');
-      expect(config?.endpoint).toBe('system_schema');
+      expect(config?.scheme).toMatchInlineSnapshot(`"cassandra"`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`"system_schema"`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
       expect(config?.hosts).toMatchInlineSnapshot(`
 Array [
   Object {
@@ -35,9 +36,9 @@ Array [
       const config = BaseDataAdapter.getConnectionParameters(
         'cassandra://username:password@localhost:9042',
       );
-      expect(config?.scheme).toBe('cassandra');
-      expect(config?.username).toBe('username');
-      expect(config?.password).toBe('password');
+      expect(config?.scheme).toMatchInlineSnapshot(`"cassandra"`);
+      expect(config?.username).toMatchInlineSnapshot(`"username"`);
+      expect(config?.password).toMatchInlineSnapshot(`"password"`);
       expect(config?.hosts).toMatchInlineSnapshot(`
 Array [
   Object {
@@ -52,10 +53,11 @@ Array [
       const config = BaseDataAdapter.getConnectionParameters(
         'cassandra://username:password@localhost:9042/system_schema',
       );
-      expect(config?.scheme).toBe('cassandra');
-      expect(config?.username).toBe('username');
-      expect(config?.password).toBe('password');
-      expect(config?.endpoint).toBe('system_schema');
+      expect(config?.scheme).toMatchInlineSnapshot(`"cassandra"`);
+      expect(config?.username).toMatchInlineSnapshot(`"username"`);
+      expect(config?.password).toMatchInlineSnapshot(`"password"`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`"system_schema"`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
       expect(config?.hosts).toMatchInlineSnapshot(`
 Array [
   Object {
@@ -65,11 +67,12 @@ Array [
 ]
 `);
     });
+
     test('input that needs to be encoded properly', async () => {
       const config = BaseDataAdapter.getConnectionParameters(
         'cassandra://sqlui-native-17823707621378612879:some_strong-PasswordMa+9T=]-G?We4Pp$wcUK==@sqlui-native-17823707621378612879.cassandra.cosmos.azure.com:10350',
       );
-      expect(config?.scheme).toBe('cassandra');
+      expect(config?.scheme).toMatchInlineSnapshot(`"cassandra"`);
       expect(config?.username).toMatchInlineSnapshot(`"sqlui-native-17823707621378612879"`);
       expect(config?.password).toMatchInlineSnapshot(
         `"some_strong-PasswordMa+9T=]-G?We4Pp$wcUK=="`,
@@ -79,6 +82,127 @@ Array [
   Object {
     "host": "sqlui-native-17823707621378612879.cassandra.cosmos.azure.com",
     "port": 10350,
+  },
+]
+`);
+    });
+
+    test('input that needs further encoding', async () => {
+      const config = BaseDataAdapter.getConnectionParameters(
+        'mongodb+srv://username:Mgvkgff8gjv6fp4ju4hag97%25t%2FX(EB%40n9)(T(7P)nm2ytsbmd2aw26ncsd54@mongodb.azure.com',
+      );
+      expect(config?.scheme).toMatchInlineSnapshot(`"mongodb+srv"`);
+      expect(config?.username).toMatchInlineSnapshot(`"username"`);
+      expect(config?.password).toMatchInlineSnapshot(
+        `"Mgvkgff8gjv6fp4ju4hag97%t/X(EB@n9)(T(7P)nm2ytsbmd2aw26ncsd54"`,
+      );
+      expect(config?.endpoint).toMatchInlineSnapshot(`undefined`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
+      expect(config?.hosts).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "host": "mongodb.azure.com",
+  },
+]
+`);
+    });
+
+    test('postgresql complex example', async () => {
+      const config = BaseDataAdapter.getConnectionParameters(
+        'postgresql://demo:demo13524@127.0.0.1:26257/movr?sslmode=require',
+      );
+      expect(config?.scheme).toMatchInlineSnapshot(`"postgresql"`);
+      expect(config?.username).toMatchInlineSnapshot(`"demo"`);
+      expect(config?.password).toMatchInlineSnapshot(`"demo13524"`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`"movr"`);
+      expect(config?.options).toMatchInlineSnapshot(`
+Object {
+  "sslmode": "require",
+}
+`);
+      expect(config?.options).toMatchInlineSnapshot(`
+Object {
+  "sslmode": "require",
+}
+`);
+      expect(config?.hosts).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "host": "127.0.0.1",
+    "port": 26257,
+  },
+]
+`);
+    });
+
+    test('mongodb+srv complex example', async () => {
+      const config = BaseDataAdapter.getConnectionParameters(
+        'mongodb+srv://username:password@localhost:27017',
+      );
+      expect(config?.scheme).toMatchInlineSnapshot(`"mongodb+srv"`);
+      expect(config?.username).toMatchInlineSnapshot(`"username"`);
+      expect(config?.password).toMatchInlineSnapshot(`"password"`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`undefined`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
+      expect(config?.hosts).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "host": "localhost",
+    "port": 27017,
+  },
+]
+`);
+    });
+
+    test('mongodb+srv with no port example', async () => {
+      const config = BaseDataAdapter.getConnectionParameters(
+        'mongodb+srv://username:password@localhost',
+      );
+      expect(config?.scheme).toMatchInlineSnapshot(`"mongodb+srv"`);
+      expect(config?.username).toMatchInlineSnapshot(`"username"`);
+      expect(config?.password).toMatchInlineSnapshot(`"password"`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`undefined`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
+      expect(config?.hosts).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "host": "localhost",
+  },
+]
+`);
+    });
+
+    test('redis simple example', async () => {
+      const config = BaseDataAdapter.getConnectionParameters('redis://localhost:6379');
+      expect(config?.scheme).toMatchInlineSnapshot(`"redis"`);
+      expect(config?.username).toMatchInlineSnapshot(`undefined`);
+      expect(config?.password).toMatchInlineSnapshot(`undefined`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`undefined`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
+      expect(config?.hosts).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "host": "localhost",
+    "port": 6379,
+  },
+]
+`);
+    });
+
+    test('rediss complex example', async () => {
+      const config = BaseDataAdapter.getConnectionParameters(
+        'rediss://username:password@localhost:6379',
+      );
+      expect(config?.scheme).toMatchInlineSnapshot(`"rediss"`);
+      expect(config?.username).toMatchInlineSnapshot(`"username"`);
+      expect(config?.password).toMatchInlineSnapshot(`"password"`);
+      expect(config?.endpoint).toMatchInlineSnapshot(`undefined`);
+      expect(config?.options).toMatchInlineSnapshot(`undefined`);
+      expect(config?.hosts).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "host": "localhost",
+    "port": 6379,
   },
 ]
 `);
