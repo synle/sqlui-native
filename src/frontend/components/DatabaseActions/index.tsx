170bbc5516bd013a6b00e0a660dfa5a0a76ead30
Sy Le <lenguyensy@gmail.com>
2022-08-15 10:33:15 -0700

    Basic form of relationship diagram with `react-flow` (#609)
    * wip
    
    * cleaned up the page layout
    
    * wip
    
    * wip more dynamics
    
    * wip
    
    * 1.61.0
    
    * CI / CD - Format Updates Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/components/DatabaseActions/index.tsx
+++ src/frontend/components/DatabaseActions/index.tsx
@@ -1,6 +1,8 @@
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import SelectAllIcon from '@mui/icons-material/SelectAll';
+import SsidChartIcon from '@mui/icons-material/SsidChart';
 import IconButton from '@mui/material/IconButton';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { getDatabaseActions } from 'src/common/adapters/DataScriptFactory';
 import DropdownButton from 'src/frontend/components/DropdownButton';
@@ -17,6 +19,7 @@ type DatabaseActionsProps = {
 };
 
 export default function DatabaseActions(props: DatabaseActionsProps): JSX.Element | null {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const querySize = useQuerySizeSetting();
   let databaseId: string | undefined = props.databaseId;
@@ -52,20 +55,43 @@ export default function DatabaseActions(props: DatabaseActionsProps): JSX.Elemen
     ...actions,
   ];
 
+  // TODO: move this into the interface of script adapter
+  // supportVisualization
+  switch (dialect) {
+    case 'mysql':
+    case 'mariadb':
+    case 'mssql':
+    case 'postgres':
+    case 'postgresql':
+    case 'sqlite':
+      actions.push({
+        label: 'Visualize',
+        description: `Visualize all tables in this database.`,
+        icon: <SsidChartIcon />,
+        //@ts-ignore
+        onClick: () => navigate(`/relationship/${connectionId}/${databaseId}`),
+      });
+      break;
+  }
+
   const options = actions.map((action) => ({
     label: action.label,
     startIcon: action.icon,
     onClick: async () =>
-      selectCommand({
-        event: 'clientEvent/query/apply',
-        data: {
-          connectionId,
-          databaseId,
-          tableId: '',
-          sql: action.query,
-        },
-        label: action.description || `Applied "${action.label}" to active query tab.`,
-      }),
+      //@ts-ignore
+      action?.onClick
+        ? //@ts-ignore
+          action.onClick()
+        : selectCommand({
+            event: 'clientEvent/query/apply',
+            data: {
+              connectionId,
+              databaseId,
+              tableId: '',
+              sql: action.query,
+            },
+            label: action.description || `Applied "${action.label}" to active query tab.`,
+          }),
   }));
 
   if (!treeActions.showContextMenu) {
---
+++ src/frontend/views/RelationshipChartPage/index.tsx
@@ -0,0 +1,104 @@
+import Box from '@mui/material/Box';
+import ReactFlow from 'react-flow-renderer';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useGetAllTableColumns } from 'src/frontend/hooks/useConnection';
+import 'src/frontend/App.scss';
+import 'src/frontend/electronRenderer';
+
+type MyNode = any;
+type MyEdge = any;
+
+export default function RelationshipChartPage() {
+  const urlParams = useParams();
+  const connectionId = urlParams.connectionId as string;
+  const databaseId = urlParams.databaseId as string;
+
+  const [nodes, setNodes] = useState<MyNode[]>([]);
+  const [edges, setEdges] = useState<MyEdge[]>([]);
+
+  const { data, isLoading } = useGetAllTableColumns(connectionId, databaseId);
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    const newNodes: MyNode[] = [];
+    const newEdges: MyEdge[] = [];
+
+    const width = 200;
+    const widthDelta = 100;
+    const height = 25;
+    const heightDelta = 100;
+
+    const mapNodeConnectionsCount: Record<string, number> = {}; // connection => count
+
+    let i = 0;
+    for (const tableName of Object.keys(data)) {
+      newNodes.push({
+        id: tableName,
+        data: { label: tableName },
+        connectable: false,
+        position: { x: width * 3 + widthDelta * 3, y: i * height + i * heightDelta },
+      });
+
+      i++;
+    }
+
+    for (const tableName of Object.keys(data)) {
+      const tableColumns = data[tableName];
+
+      for (const tableColumn of tableColumns) {
+        if (tableColumn.referencedColumnName && tableColumn.referencedTableName) {
+          const foundEdge = newEdges.find(
+            (edge) => edge.source === tableName && edge.target === tableColumn.referencedTableName,
+          );
+
+          newEdges.push({
+            id: `${tableName}.${tableColumn.name} => ${tableColumn.referencedTableName}.${tableColumn.referencedColumnName}`,
+            source: tableName,
+            target: tableColumn.referencedTableName,
+            // label: `${tableColumn.name} => ${tableColumn.referencedTableName}.${tableColumn.referencedColumnName}`,
+            type: 'straight',
+          });
+
+          mapNodeConnectionsCount[tableColumn.referencedTableName] =
+            mapNodeConnectionsCount[tableColumn.referencedTableName] || 0;
+          mapNodeConnectionsCount[tableColumn.referencedTableName]++;
+        }
+      }
+    }
+
+    const countGroups = [...new Set(Object.values(mapNodeConnectionsCount))]
+      .map((s) => s as number)
+      .sort((a, b) => b - a);
+    const [firstGroup, secondGroup, thirdGroup] = countGroups;
+
+    for (const node of newNodes) {
+      const tableName = node.id;
+      const count = mapNodeConnectionsCount[tableName];
+
+      if (count === firstGroup) {
+        node.position.x = width * 0 + widthDelta * 0;
+      } else if (count === secondGroup) {
+        node.position.x = width * 1 + widthDelta * 1;
+      } else if (count === thirdGroup) {
+        node.position.x = width * 2 + widthDelta * 2;
+      }
+    }
+
+    setNodes(newNodes);
+    setEdges(newEdges);
+  }, [JSON.stringify(data)]);
+
+  if (isLoading) {
+    return <Box sx={{ padding: 2 }}>Loading...</Box>;
+  }
+
+  return (
+    <div style={{ height: 'calc(100vh - 50px)' }}>
+      <ReactFlow defaultNodes={nodes} defaultEdges={edges} fitView />
+    </div>
+  );
+}
