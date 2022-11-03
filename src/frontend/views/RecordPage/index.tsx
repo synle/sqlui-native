5936b5e642d1cce65a643858b0261a8bf29f3a20
Sy Le <lenguyensy@gmail.com>
2022-11-03 06:49:51 -0700

    Sort order of the column headers always sorted alphabetically (#699)
    * persist and use the sort order from the actual api
    
    * CI / CD - Format Updates Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/views/RecordPage/index.tsx
+++ src/frontend/views/RecordPage/index.tsx
@@ -49,7 +49,6 @@ import {
 import useToaster from 'src/frontend/hooks/useToaster';
 import { useTreeActions } from 'src/frontend/hooks/useTreeActions';
 import LayoutTwoColumns from 'src/frontend/layout/LayoutTwoColumns';
-import { sortColumnNamesForUnknownData } from 'src/frontend/utils/commonUtils';
 import { formatJS, formatSQL } from 'src/frontend/utils/formatter';
 import { SqluiCore, SqluiFrontend } from 'typings';
 
@@ -79,7 +78,7 @@ type RecordFormReponse = {
  */
 function RecordView(props: RecordDetailsPageProps): JSX.Element | null {
   const { data } = props;
-  const columnNames = sortColumnNamesForUnknownData(Object.keys(data || {}));
+  const columnNames = Object.keys(data || {});
 
   return (
     <>
