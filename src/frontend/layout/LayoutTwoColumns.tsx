a0135861ced36bad3ac61d86aa8955e5a1847c3d
Sy Le <lenguyensy@gmail.com>
2023-05-11 10:02:29 -0700

    Toggle the left pane (used for the schema currently) (#730)
    * allow user to show or hide the left pane (used for the schema currently)
    
    * CI / CD - Format Updates Automatic Commit
    
    ---------
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/layout/LayoutTwoColumns.tsx
+++ src/frontend/layout/LayoutTwoColumns.tsx
@@ -1,5 +1,8 @@
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import Fab from '@mui/material/Fab';
 import { Bar, Container, Section } from 'react-simple-resizer';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSideBarWidthPreference } from 'src/frontend/hooks/useClientSidePreference';
 import { useTreeActions } from 'src/frontend/hooks/useTreeActions';
 
@@ -12,6 +15,13 @@ export default function LayoutTwoColumns(props: LayoutTwoColumnsProps): JSX.Elem
   const { className = '', children } = props;
   const { value: width, onChange: onSetWidth } = useSideBarWidthPreference();
   const { setTreeActions } = useTreeActions();
+  const [leftPaneExpanded, setLeftPaneExpanded] = useState(true);
+
+  const fabStyle = {
+    position: 'fixed',
+    bottom: '1rem',
+    left: '1.5rem',
+  };
 
   useEffect(() => {
     setTreeActions({
@@ -19,15 +29,29 @@ export default function LayoutTwoColumns(props: LayoutTwoColumnsProps): JSX.Elem
     });
   }, [setTreeActions]);
 
+  if (leftPaneExpanded) {
+    return (
+      <Container className={`${className} LayoutTwoColumns`}>
+        <Fab size='small' sx={fabStyle} onClick={() => setLeftPaneExpanded(false)}>
+          <KeyboardArrowLeftIcon />
+        </Fab>
+        <Section defaultSize={width} onSizeChanged={onSetWidth} minSize={250} maxSize={600}>
+          <div className='LayoutTwoColumns__LeftPane'>{children[0]}</div>
+        </Section>
+        <Bar size={10} className='Resizer Resizer--Horizontal' />
+        <Section>
+          <div className='LayoutTwoColumns__RightPane'>{children[1]}</div>
+        </Section>
+      </Container>
+    );
+  }
+
   return (
-    <Container className={`${className} LayoutTwoColumns`}>
-      <Section defaultSize={width} onSizeChanged={onSetWidth} minSize={250} maxSize={600}>
-        <div className='LayoutTwoColumns__LeftPane'>{children[0]}</div>
-      </Section>
-      <Bar size={10} className='Resizer Resizer--Horizontal' />
-      <Section>
-        <div className='LayoutTwoColumns__RightPane'>{children[1]}</div>
-      </Section>
-    </Container>
+    <>
+      <Fab size='small' sx={fabStyle} onClick={() => setLeftPaneExpanded(true)}>
+        <KeyboardArrowRightIcon />
+      </Fab>
+      <div className='LayoutTwoColumns__RightPane'>{children[1]}</div>
+    </>
   );
 }
