ac35b2875fd98edf43a489f2bfc20c1a4b0f156e
Sy Le <lenguyensy@gmail.com>
2023-05-11 14:23:18 -0700

    combine context provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,24 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import React from 'react';
 import App from 'src/frontend/App';
-import ActionDialogsContext from 'src/frontend/hooks/useActionDialogs';
-import ConnectionQueryContext from 'src/frontend/hooks/useConnectionQuery';
-import SettingProvider from 'src/frontend/hooks/useSetting';
-import ShowHideContext from 'src/frontend/hooks/useShowHide';
-import TreeActionContext from 'src/frontend/hooks/useTreeActions';
+import ActionDialogsContextProvider from 'src/frontend/hooks/useActionDialogs';
+import ConnectionQueryContextProvider from 'src/frontend/hooks/useConnectionQuery';
+import SettingContextProvider from 'src/frontend/hooks/useSetting';
+import ShowHideContextProvider from 'src/frontend/hooks/useShowHide';
+import TreeActionContextProvider from 'src/frontend/hooks/useTreeActions';
+
+const CombinedContextProvider = ({ children }) => {
+  return [
+    ActionDialogsContextProvider,
+    ConnectionQueryContextProvider,
+    TreeActionContextProvider,
+    ShowHideContextProvider,
+    SettingContextProvider,
+  ].reduceRight(
+    (acc, Provider) => <Provider>{acc}</Provider>,
+    children
+  );
+}
 
 const renderApp = function () {
   const queryClient = new QueryClient({
@@ -27,17 +40,9 @@ const renderApp = function () {
         <CssBaseline />
         <ReactQueryDevtools initialIsOpen={false} />
 
-        <ActionDialogsContext>
-          <ConnectionQueryContext>
-            <TreeActionContext>
-              <ShowHideContext>
-                <SettingProvider>
-                  <App />
-                </SettingProvider>
-              </ShowHideContext>
-            </TreeActionContext>
-          </ConnectionQueryContext>
-        </ActionDialogsContext>
+        <CombinedContextProvider>
+          <App />
+        </CombinedContextProvider>
       </QueryClientProvider>
     </SnackbarProvider>,
     document.querySelector('#body'),
