0c066571254d4765dda95e6ab78328d7e2a7c132
Sy Le <lenguyensy@gmail.com>
2023-05-13 11:04:25 -0700

    No longer use mui table for legacy table and consolidate it with modern table (#750)
    * cleaned up and rewrite legacy table
    
    * CI / CD - Format Updates Automatic Commit
    
    ---------
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/frontend/components/DataTable/ModernDataTable.tsx
+++ src/frontend/components/DataTable/ModernDataTable.tsx
@@ -3,8 +3,6 @@ import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ZoomOutMapIcon from '@mui/icons-material/ZoomOutMap';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
 import Tooltip from '@mui/material/Tooltip';
 import { useVirtualizer } from '@tanstack/react-virtual';
 import {
@@ -16,108 +14,24 @@ import {
   useSortBy,
   useTable,
 } from 'react-table';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { DataTableProps } from 'src/frontend/components/DataTable';
+import {
+  ColumnResizer,
+  defaultTableHeight,
+  StyledDivContainer,
+  StyledDivContentRowForVirualized as StyledDivContentRow,
+  StyledDivHeaderCellForVirtualized as StyledDivHeaderCell,
+  StyledDivHeaderCellLabel,
+  StyledDivHeaderRow,
+  StyledDivValueCellForVirtualized as StyledDivValueCell,
+  tableCellHeaderHeight,
+  tableCellHeight,
+  tableCellWidth,
+} from 'src/frontend/components/DataTable/DataTableComponents';
 import { GlobalFilter, SimpleColumnFilter } from 'src/frontend/components/DataTable/Filter';
 import DropdownMenu from 'src/frontend/components/DropdownMenu';
 import { useAddDataSnapshot } from 'src/frontend/hooks/useDataSnapshot';
-
-const defaultTableHeight = '40vh';
-
-const tableCellHeaderHeight = 75;
-
-const tableCellHeight = 35;
-
-const tableCellWidth = 160;
-
-const StyledDivContainer = styled('div')(({ theme }) => ({}));
-
-const StyledDivHeaderRow = styled('div')(({ theme }) => ({
-  fontWeight: 'bold',
-  display: 'flex',
-  alignItems: 'center',
-  flexWrap: 'nowrap',
-  minWidth: '100%',
-  position: 'sticky',
-  top: 0,
-  left: 0,
-  zIndex: theme.zIndex.drawer + 1,
-  backgroundColor: theme.palette.background.default,
-  color: theme.palette.text.primary,
-  boxSizing: 'border-box',
-  fontSize: '1 rem',
-
-  '> div': {
-    height: `${tableCellHeaderHeight}px`,
-    backgroundColor: theme.palette.background.default,
-    color: theme.palette.text.primary,
-    paddingTop: '5px',
-    boxSizing: 'border-box',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    wordBreak: 'break-all',
-    whiteSpace: 'nowrap',
-  },
-}));
-
-const StyledDivContentRow = styled('div')(({ theme }) => ({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  display: 'flex',
-  alignItems: 'center',
-  userSelect: 'none',
-  minWidth: '100%',
-  backgroundColor: theme.palette.action.selected,
-  boxSizing: 'border-box',
-  fontSize: '0.95 rem',
-
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.focus,
-  },
-
-  '&:hover': {
-    backgroundColor: theme.palette.action.hover,
-  },
-}));
-
-const StyledDivHeaderCell = styled('div')(({ theme }) => ({
-  flexShrink: 0,
-  height: `${tableCellHeight}px`,
-  paddingInline: '0.5rem',
-}));
-
-const StyledDivHeaderCellLabel = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  '> span': {
-    flexGrow: 1,
-  },
-}));
-
-const StyledDivValueCell = styled('div')(({ theme }) => ({
-  flexShrink: 0,
-  height: `${tableCellHeight}px`,
-  paddingInline: '0.5rem',
-  display: 'flex',
-  alignItems: 'center',
-  paddingTop: '7px',
-}));
-
-const ColumnResizer = styled('div')(({ theme }) => ({
-  background: theme.palette.text.primary,
-  cursor: 'col-resize',
-  userSelect: 'none',
-  height: '100%',
-  width: '8px',
-  position: 'absolute',
-  right: '0',
-  top: '0',
-  opacity: 0.05,
-  '&:hover': {
-    opacity: 1,
-  },
-}));
 
 export default function ModernDataTable(props: DataTableProps): JSX.Element | null {
   const { columns, data } = props;
@@ -141,16 +55,7 @@ export default function ModernDataTable(props: DataTableProps): JSX.Element | nu
 
   const allRecordSize = data.length;
   const pageSizeToUse = allRecordSize;
-  const {
-    getTableBodyProps,
-    gotoPage,
-    headerGroups,
-    page,
-    prepareRow,
-    setGlobalFilter,
-    setPageSize,
-    state,
-  } = useTable(
+  const { headerGroups, page, prepareRow, setGlobalFilter } = useTable(
     {
       initialState: {
         pageSize: pageSizeToUse,
@@ -170,18 +75,6 @@ export default function ModernDataTable(props: DataTableProps): JSX.Element | nu
     useBlockLayout,
     useResizeColumns,
   );
-  const { pageIndex, pageSize } = state;
-
-  const onPageChange = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement> | null, page: number) => {
-      gotoPage(page);
-    },
-    [],
-  );
-
-  const onRowsPerPageChange = useCallback((e) => {
-    setPageSize(e.target.value);
-  }, []);
 
   const onRowContextMenuClick = (e: React.SyntheticEvent) => {
     const target = e.target as HTMLElement;
@@ -306,7 +199,9 @@ export default function ModernDataTable(props: DataTableProps): JSX.Element | nu
                       ))}
                     {/* Render the column resize handler */}
                   </StyledDivHeaderCellLabel>
-                  {column.canFilter && <Box sx={{ mt: 1 }}>{column.render('Filter')}</Box>}
+                  {column.canFilter && column.Header && (
+                    <Box sx={{ mt: 1 }}>{column.render('Filter')}</Box>
+                  )}
                   {column.canResize && (
                     <ColumnResizer
                       {...column.getResizerProps()}
