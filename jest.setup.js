a6f7d3cbb6d7bc93604148b88d5a4ee32cfeda13
Sy Le <lenguyensy@gmail.com>
2026-02-17 11:25:29 -0800

    fixed tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,8 @@
+// polyfill global crypto for Node < 19 (required by @typespec/ts-http-runtime)
+const { webcrypto } = require('crypto');
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
+
 // set timeout for tests...
 jest.setTimeout(10000);
