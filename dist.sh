3cd749a1f911db7fae6996588c7676b23a7d2a4c
Sy Le <lenguyensy@gmail.com>
2026-02-17 16:31:31 -0800

    dist update

--- dist.sh
+++ dist.sh
@@ -1,23 +1,4 @@
 bash build.sh
 
-node -e "
-const fs = require('fs');
-const path = require('path');
-
-function copyDir(src, dest) {
-  for (const entry of fs.readdirSync(src, { withFileTypes: true })) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
-    if (entry.isDirectory()) {
-      fs.mkdirSync(destPath, { recursive: true });
-      copyDir(srcPath, destPath);
-    } else {
-      fs.copyFileSync(srcPath, destPath);
-    }
-  }
-}
-
-copyDir('build', '.');
-console.log('Copied: build/**/* -> .');
-"
+node -e "require('fs').cpSync('build', '.', { recursive: true }); console.log('Copied: build/**/* -> .');"
 npm run dist
