0538bbc089a619263a8dce3140ff8dc6600ffa63
Sy Le <lenguyensy@gmail.com>
2026-02-18 10:53:33 -0800

    Revert "consolidate usequery"
    This reverts commit 07af139a500ca29b0961bdcd097fe62b4aa5f0b7.

--- src/frontend/hooks/useDataSnapshot.tsx
+++ src/frontend/hooks/useDataSnapshot.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from 'src/frontend/utils/reactQueryUtils';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import dataApi from 'src/frontend/data/api';
 import { SqluiCore } from 'typings';
 
