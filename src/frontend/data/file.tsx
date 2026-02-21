99ed3ca21172126bdfe3361f16325179f9000fab
Sy Le <lenguyensy@gmail.com>
2026-02-20 23:37:40 -0800

    fixed an issue where sometimes the session modal selection is not showing
    CI / CD - Format code

--- src/frontend/data/file.tsx
+++ src/frontend/data/file.tsx
@@ -1,32 +1,32 @@
-import CsvEngine from 'json-2-csv';
+import CsvEngine from "json-2-csv";
 
-export function downloadText(downloadFileName: string, content: string, mimeType = 'text/csv') {
+export function downloadText(downloadFileName: string, content: string, mimeType = "text/csv") {
   let encodedContent = `data:${mimeType};charset=utf-8,${content}`;
   let encodedUri = encodeURI(encodedContent);
-  let link = document.createElement('a');
-  link.setAttribute('href', encodedUri);
-  link.setAttribute('download', downloadFileName);
+  let link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", downloadFileName);
   document.body.appendChild(link); // Required for FF
 
   link.click(); // This will download the data file named "my_data.csv".
 }
 
 export function downloadJSON(downloadFileName: string, data) {
-  downloadText(downloadFileName, JSON.stringify(data, null, 2), 'text/json');
+  downloadText(downloadFileName, JSON.stringify(data, null, 2), "text/json");
 }
 
 export function downloadCsv(downloadFileName: string, data) {
   CsvEngine.json2csv(data, (err, newCsv) => {
     if (!err && newCsv) {
-      downloadText(downloadFileName, newCsv, 'text/csv');
+      downloadText(downloadFileName, newCsv, "text/csv");
     }
   });
 }
 
 export function downloadBlob(downloadFileName: string, blobContent: string) {
-  let link = document.createElement('a');
-  link.setAttribute('href', blobContent);
-  link.setAttribute('download', downloadFileName);
+  let link = document.createElement("a");
+  link.setAttribute("href", blobContent);
+  link.setAttribute("download", downloadFileName);
   document.body.appendChild(link); // Required for FF
 
   link.click();
--- src/frontend/data/session.tsx
+++ src/frontend/data/session.tsx
@@ -1,19 +1,19 @@
-import { SessionStorageConfig } from 'src/frontend/data/config';
-import { getGeneratedRandomId } from 'src/frontend/utils/commonUtils';
+import { SessionStorageConfig } from "src/frontend/data/config";
+import { getGeneratedRandomId } from "src/frontend/utils/commonUtils";
 
 export function getRandomSessionId() {
   return getGeneratedRandomId(`sessionId`);
 }
 
 export function setCurrentSessionId(newSessionId: string, suppressReload = false) {
-  const currentWindowId = sessionStorage.getItem('sqlui-native.windowId') || '';
+  const currentWindowId = sessionStorage.getItem("sqlui-native.windowId") || "";
 
   // clear current configs
   SessionStorageConfig.clear();
 
   // set the new sessionId
-  sessionStorage.setItem('sqlui-native.sessionId', newSessionId);
-  sessionStorage.setItem('sqlui-native.windowId', currentWindowId);
+  sessionStorage.setItem("sqlui-native.sessionId", newSessionId);
+  sessionStorage.setItem("sqlui-native.windowId", currentWindowId);
 
   // reload the page
   if (suppressReload === false) {
