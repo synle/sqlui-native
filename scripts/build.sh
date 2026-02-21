d75c992eac061b0b013e0a0e88dee3e5d399ab01
Sy Le <lenguyensy@gmail.com>
2026-02-21 08:18:18 -0800

    added format check and fix up the dist linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,30 +1,15 @@
 run_step() {
   echo """
-===================================================
-# $1
-===================================================
-"""
+  ===================================================
+  # $1
+  ===================================================
+  """
   eval "$1"
 }
 
-input_lower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-if [[ "$input_lower" == "true" || "$input_lower" == "1" ]]; then
-  # NOTE: used for deployment / packaging
-  run_step "npm ci || npm install"
-  run_step "npm version --no-git-tag-version patch"
-  run_step "node scripts/prebuild.js"
-  run_step "npm run build"
-  run_step "node scripts/postbuild.js"
-  # run_step "npm run test-ci"  # [Optional] here because we already run the test in prebuild
-  # run_step "npm run typecheck"  # [Optional] here because we already run the test in prebuild
-else
-  # NOTE: for prebuild, used to check everything
-  set -e
-  run_step "npm ci || npm install"
-  run_step "node scripts/prebuild.js"
-  run_step "npm run build"
-  run_step "node scripts/postbuild.js"
-  run_step "npm run test-ci"
-  run_step "npm run typecheck"
-fi
-
+# NOTE: used for deployment / packaging
+run_step "npm ci || npm install"
+run_step "npm version --no-git-tag-version patch"
+run_step "node scripts/prebuild.js"
+run_step "npm run build"
+run_step "node scripts/postbuild.js"
