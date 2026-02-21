c1a7293c5b51b2454408a98842ab34bf3864becb
Sy Le <lenguyensy@gmail.com>
2026-02-21 14:11:03 -0800

    code format

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,4 +7,4 @@ source "$(dirname "$0")/common.sh"
 ####################################
 run_step "npm ci || npm install"
 run_step "npm run lint"
-run_step "npm run format"
+run_step "npm run format:ci"
