a495b6d118e1d7179330b66e61f850d1a9b4d61d
Sy Le <lenguyensy@gmail.com>
2026-02-21 19:34:40 -0800

    streamline config with less deps

--- src/electron/polyfills.ts
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-
-// Polyfill globalThis.crypto for packages like @typespec/ts-http-runtime
-// that expect the Web Crypto API to be available globally (used by @azure/data-tables).
-if (!globalThis.crypto) {
-  // @ts-ignore - Node's crypto module is compatible enough for randomUUID
-  globalThis.crypto = crypto;
-}
