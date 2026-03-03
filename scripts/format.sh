a97b67a299f9fa0677132947aad91ba86fc60471
Sy Le <lenguyensy@gmail.com>
2026-03-03 03:08:59 -0800

    Update format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-source "$(dirname "$0")/common.sh"
-
 ####################################
 # format.sh (used for code format)
 ####################################
-run_step "npm ci || npm install"
-run_step "npm run lint"
-run_step "npm run format:ci"
+npm ci || npm install
+npm run lint
+npm run format:ci
