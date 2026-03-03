7e5e3bce03a746ff6edffb11b2e548414a875808
Sy Le <lenguyensy@gmail.com>
2026-03-03 08:05:02 -0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source "$(dirname "$0")/common.sh"
-
 ####################################
 # test.sh (used for testing)
 ####################################
 set -e
-run_step "npm ci || npm install"
-run_step "npm run test-ci"
-run_step "npm run typecheck"
-run_step "npm run lint"
+pm ci || npm install
+npm run test-ci
+npm run typecheck
+npm run lint
