494cd3075e7e7086f14dcb8eee86ceaaccd6a8cd
Sy Le <lenguyensy@gmail.com>
2026-03-09 17:11:08 -0700

    update the toast history to be 70vw wide, instead of just 50% ish

--- src/frontend/hooks/useToaster.tsx
+++ src/frontend/hooks/useToaster.tsx
@@ -296,8 +296,7 @@ function ToastItem({ toast }: { toast: InternalToast }) {
         borderRadius: "4px",
         boxShadow: "0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)",
         fontSize: "0.875rem",
-        minWidth: "280px",
-        maxWidth: "500px",
+        minWidth: "70vw",
       }}
     >
       <span style={{ flex: 1 }}>{toast.message}</span>
