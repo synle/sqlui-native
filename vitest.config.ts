1f4505795f5e8b307795afcf858bd786eab032f6
Sy Le <lenguyensy@gmail.com>
2026-03-21 13:24:06 -0700

    minor

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,12 +18,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "json-summary"],
       include: ["src/**/*.{ts,tsx}"],
-      exclude: [
-        "**/*.spec.{ts,tsx}",
-        "**/*.integration.spec.{ts,tsx}",
-        "**/_SampleDataAdapter_/**",
-        "**/node_modules/**",
-      ],
+      exclude: ["**/*.spec.{ts,tsx}", "**/*.integration.spec.{ts,tsx}", "**/_SampleDataAdapter_/**", "**/node_modules/**"],
     },
   },
 });
