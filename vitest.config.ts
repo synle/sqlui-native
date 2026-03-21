1c02176741a8dbefa1bedab77cc17613862c8df6
Sy Le <lenguyensy@gmail.com>
2026-03-21 13:21:25 -0700

    hook up code coverage summary job and fixed issue with package.yml

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,5 +14,16 @@ export default defineConfig({
     include: ["**/*.spec.{ts,tsx}"],
     exclude: ["**/*.integration.spec.{ts,tsx}", "**/node_modules/**", "**/_SampleDataAdapter_/**"],
     setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary"],
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "**/*.spec.{ts,tsx}",
+        "**/*.integration.spec.{ts,tsx}",
+        "**/_SampleDataAdapter_/**",
+        "**/node_modules/**",
+      ],
+    },
   },
 });
