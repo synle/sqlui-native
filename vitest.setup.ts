bcb432853e8aee626c3c550e978cd0487168f0f8
Sy Le <lenguyensy@gmail.com>
2026-03-20 23:09:12 -0700

    sfdc integraiton tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,3 +3,17 @@ import { webcrypto } from "crypto";
 if (!globalThis.crypto) {
   (globalThis as any).crypto = webcrypto;
 }
+
+// polyfill node-fetch AbortError (not exported in v2, but jsforce references it at request.js:183)
+import * as nodeFetch from "node-fetch";
+if (!(nodeFetch as any).AbortError) {
+  class AbortError extends Error {
+    type: string;
+    constructor(message: string) {
+      super(message);
+      this.type = "aborted";
+      this.name = "AbortError";
+    }
+  }
+  (nodeFetch as any).AbortError = AbortError;
+}
